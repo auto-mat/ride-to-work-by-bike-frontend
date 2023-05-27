00c94afa85a7521660b0cc389df369dfd94b01df
Šimon Macek <simon.macek@proficio.cz>
2023-05-27 16:00:53 +0200

    card challenges

--- src/components/__tests__/VueCardChallenge.cy.js
+++ src/components/__tests__/VueCardChallenge.cy.js
@@ -1,11 +1,9 @@
 import VueCardChallenge from 'components/VueCardChallenge.vue';
-import { i18n } from '../../boot/i18n';
 
 describe('<VueCardChallenge>', () => {
   const title = 'Challenge 1';
   const url = '#';
   const dates = '1. říj.–31. říj. 2022';
-  const image = 'https://picsum.photos/500/550';
   const company = true;
 
   beforeEach(() => {
@@ -15,111 +13,52 @@ describe('<VueCardChallenge>', () => {
           title,
           url,
           dates,
-          image,
           company,
         },
       },
     });
   });
 
-  it('has translation for all strings', () => {
-    const translationStrings = ['dates', 'company'];
-
-    const translationKeyList = translationStrings.map(
-      (item) => `index.cardChallenge.${item}`
-    );
-
-    translationKeyList.forEach((translationKey) => {
-      const defaultEnglishString = i18n.global.t(translationKey, 'en');
-
-      const locales = i18n.global.availableLocales;
-      locales
-        .filter((locale) => locale !== 'en')
-        .forEach((locale) => {
-          i18n.global.locale = locale;
-          const translatedString = i18n.global.t(translationKey);
-
-          cy.wrap(translatedString)
-            .should('be.a', 'string')
-            .and('not.equal', defaultEnglishString);
-        });
-    });
-  });
-
-  it('renders title with icon on gray background', () => {
+  it('renders title', () => {
     cy.window().then(() => {
-      cy.dataCy('card-title')
+      cy.get('[data-testid="card-title"]')
         .should('be.visible')
-        .should('have.backgroundColor', 'rgba(0, 0, 0, 0.47)')
         .should('contain', title);
-
-      cy.dataCy('card-title')
-        .find('i')
-        .should('be.visible')
-        .should('contain', 'person');
-
-      cy.dataCy('card-title')
-        .find('a')
-        .should('have.css', 'font-size', '16px')
-        .should('have.css', 'font-weight', '700')
-        .then(($title) => {
-          expect($title.text()).to.equal(title);
-        });
     });
   });
 
-  it('renders title link', () => {
+  it('renders link', () => {
     cy.window().then(() => {
-      cy.dataCy('card-link')
+      cy.get('[data-testid="card-link"]')
         .should('be.visible')
         .should('have.attr', 'href', url);
     });
   });
 
-  it('renders image', () => {
-    cy.window().then(() => {
-      cy.dataCy('card')
-        .find('img')
-        .should('be.visible')
-        .then(($img) => {
-          const naturalHeight = $img[0].naturalHeight;
-          expect(naturalHeight).to.be.greaterThan(0);
-          expect($img.attr('src')).to.equal(image);
-        });
-    });
-  });
-
   it('renders dates', () => {
     cy.window().then(() => {
-      cy.dataCy('card-dates')
+      cy.get('[data-testid="card-dates"]')
         .should('be.visible')
-        .should('have.color', '#ffffff')
-        .should('have.css', 'font-size', '14px')
         .should('contain', dates);
     });
   });
 
   it('renders company badge', () => {
     cy.window().then(() => {
-      cy.dataCy('card-company-wrapper')
+      cy.get('[data-testid="card-company"]')
         .should('be.visible')
-        .should('have.css', 'position', 'absolute')
-        .should('have.css', 'top', '-12px');
-
-      cy.dataCy('card-company')
-        .should('be.visible')
-        .should('have.css', 'border-radius', '12px')
-        .should('have.css', 'font-size', '12px')
-        .should('have.css', 'font-weight', '400')
-        .should('have.color', '#ffffff')
+        .should('have.css', 'border-radius', '9999px')
+        .should('have.css', 'color', '#fff')
         .should('have.css', 'height', '24px')
-        .should('contain.text', i18n.global.t('index.cardChallenge.company'));
+        .should('have.css', 'position', 'absolute')
+        .should('have.css', 'top', '-12px')
+        .should('contain', 'Company challange');
     });
   });
 
   it('has rounded corners', () => {
     cy.window().then(() => {
-      cy.dataCy('card')
+      cy.get('[data-testid="card"]')
         .should('be.visible')
         .should('have.css', 'border-radius', '20px');
     });
