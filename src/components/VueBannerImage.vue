10cb0d1c8453fac93655167176c8ca722b6fabf8
Šimon Macek <simon.macek@proficio.cz>
2023-05-30 19:46:50 +0200

    add HP banner image

--- src/components/VueBannerImage.vue
+++ src/components/VueBannerImage.vue
@@ -1,14 +1,6 @@
 <script lang="ts">
-import { setCssVar } from 'quasar';
-import { defineComponent } from 'vue';
-
-// import types
-import { BannerImage } from 'components/types';
-
-const rideToWorkByBikeConfig: object = JSON.parse(
-  process.env.RIDE_TO_WORK_BY_BIKE_CONFIG
-);
-setCssVar('gray-light', rideToWorkByBikeConfig.colorGrayLight);
+import { defineComponent } from "vue";
+import { BannerImage } from "components/types";
 
 export default defineComponent({
   name: 'VueBannerImage',
@@ -16,73 +8,38 @@ export default defineComponent({
     banner: {
       type: Object as () => BannerImage,
       required: true,
-    },
+    }
   },
   setup() {
-    return {};
-  },
-});
+    return {}
+  }
+})
 </script>
 
 <template>
   <div>
-    <q-card :flat="true" class="rounded-20 row" data-cy="banner">
-      <q-img
-        :src="banner?.image"
-        :ratio="3 / 1"
-        class="col-sm-6"
-        data-cy="banner-half"
-      />
-      <div
-        class="col-sm-6 flex items-center q-px-md q-py-lg"
-        data-cy="banner-half"
-      >
+    <q-card :dark="true" :flat="true" :bordered="true" class="rounded-20 row" data-testid="banner">
+      <q-img :src="banner?.image" :ratio="3 / 1" class="col-6" data-testid="banner-half" />
+      <div class="col-6 flex items-center q-px-md q-py-lg" data-testid="banner-half">
         <div>
-          <div
-            v-if="banner.title"
-            class="text-dark text-subtitle1"
-            data-cy="banner-title"
-          >
-            {{ banner.title }}
+          <div v-if="banner.title" class="text-dark text-subtitle1" data-testid="banner-title">{{ banner.title }}
           </div>
-          <div
-            v-if="banner.perex"
-            class="text-dark text-caption q-mt-sm"
-            data-cy="banner-perex"
-          >
-            {{ banner.perex }}
-          </div>
+          <div v-if="banner.perex" class="text-dark text-caption" data-testid="banner-perex">{{ banner.perex }}</div>
         </div>
       </div>
     </q-card>
   </div>
 </template>
 
-<style scoped lang="scss">
+<style scoped>
 .rounded-20 {
   border-radius: 20px;
-  background-color: var(--q-gray-light);
 }
 
-.q-card > div:first-child {
+.q-card>div:first-child {
   border-top-left-radius: inherit;
-  border-top-right-radius: inherit;
-  border-bottom-left-radius: 0;
+  border-bottom-left-radius: inherit;
+  border-top-right-radius: 0;
   border-bottom-right-radius: 0;
-}
-
-@media (min-width: $breakpoint-sm-min) {
-  .q-card > div:first-child {
-    border-top-left-radius: inherit;
-    border-top-right-radius: 0;
-    border-bottom-left-radius: inherit;
-    border-bottom-right-radius: 0;
-  }
-}
-
-@media (min-width: $breakpoint-lg-min) {
-  .q-card > div:last-child {
-    padding: 48px;
-  }
 }
 </style>
--- src/components/__tests__/VueBannerImage.cy.js
+++ src/components/__tests__/VueBannerImage.cy.js
@@ -1,7 +1,5 @@
 import VueBannerImage from 'components/VueBannerImage.vue';
 
-const config = JSON.parse(process.env.RIDE_TO_WORK_BY_BIKE_CONFIG);
-
 describe('<VueBannerImage>', () => {
   const title = 'Fill in our questionnaire and win one of our great prizes!';
   const perex =
@@ -20,31 +18,20 @@ describe('<VueBannerImage>', () => {
     });
   });
 
-  it('renders two columns', () => {
-    cy.window().then(() => {
-      cy.dataCy('banner')
-        .should('be.visible')
-        .should('have.css', 'display', 'flex')
-        .should('have.css', 'flex-wrap', 'wrap');
-
-      cy.dataCy('banner-half').should('have.length', 2).should('be.visible');
-    });
-  });
-
   it('renders title', () => {
     cy.window().then(() => {
-      cy.dataCy('banner-title')
+      cy.get('[data-testid="banner-title"]')
         .should('be.visible')
         .should('contain.text', title)
-        .then(($title) => {
-          expect($title.text()).to.equal(title);
+        .then((titleNode) => {
+          expect(titleNode.text()).to.equal(title);
         });
     });
   });
 
   it('renders perex', () => {
     cy.window().then(() => {
-      cy.dataCy('banner-perex')
+      cy.get('[data-testid="banner-perex"]')
         .should('be.visible')
         .should('contain.text', perex)
         .then((perexNode) => {
@@ -53,44 +40,19 @@ describe('<VueBannerImage>', () => {
     });
   });
 
-  it('renders image', () => {
+  it('renders two columns', () => {
     cy.window().then(() => {
-      cy.dataCy('banner')
-        .find('img')
-        .should('be.visible')
-        .then(($img) => {
-          const naturalHeight = $img[0].naturalHeight;
-          expect(naturalHeight).to.be.greaterThan(0);
-          expect($img.attr('src')).to.equal(image);
-        });
-    });
-  });
-
-  it('has correct background color', () => {
-    cy.window().then(() => {
-      cy.dataCy('banner').should('have.backgroundColor', config.colorGrayLight);
+      cy.get('[data-testid="banner-half"]')
+        .should('have.length', 2)
+        .should('be.visible');
     });
   });
 
   it('has rounded corners', () => {
     cy.window().then(() => {
-      cy.dataCy('banner')
+      cy.get('[data-testid="banner"]')
         .should('be.visible')
         .should('have.css', 'border-radius', '20px');
-
-      cy.viewport('iphone-6');
-
-      cy.dataCy('banner-half')
-        .first()
-        .should('have.css', 'border-top-left-radius', '20px')
-        .should('have.css', 'border-top-right-radius', '20px');
-
-      cy.viewport('macbook-13');
-
-      cy.dataCy('banner-half')
-        .first()
-        .should('have.css', 'border-top-left-radius', '20px')
-        .should('have.css', 'border-bottom-left-radius', '20px');
     });
   });
 });
