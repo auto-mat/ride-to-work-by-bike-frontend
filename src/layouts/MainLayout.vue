6d7682f70ee9dda7c8a6a4fad4fcb4bbdff99627
Šimon Macek <simon.macek@proficio.cz>
2023-06-10 16:03:02 +0200

    layout: edit drawer width

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -19,3 +19,32 @@ export default defineComponent({
   },
 });
 </script>
+
+<template>
+  <q-layout view="hHh lpR fFf">
+    <q-drawer
+      show-if-above
+      v-model="leftDrawerOpen"
+      side="left"
+      class="bg-gray-light q-py-lg q-px-lg"
+    >
+      <vue-drawer-header></vue-drawer-header>
+      <vue-user-select class="q-pt-lg"></vue-user-select>
+      <vue-drawer-menu class="q-pt-lg"></vue-drawer-menu>
+    </q-drawer>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+
+    <q-footer>
+      <!-- footer content -->
+    </q-footer>
+  </q-layout>
+</template>
+
+<style>
+.bg-gray-light {
+  background-color: var(--q-gray-light);
+}
+</style>
