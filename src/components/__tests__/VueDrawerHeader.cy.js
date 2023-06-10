4925f751528b7e19d3a8874d1aa5337697221527
Šimon Macek <simon.macek@proficio.cz>
2023-06-10 16:02:50 +0200

    drawer header tests

---
+++ src/components/__tests__/VueDrawerHeader.cy.js
@@ -0,0 +1,36 @@
+import { hexToRgb } from 'app/test/cypress/utils';
+import VueDrawerHeader from '../VueDrawerHeader.vue';
+
+describe('<VueDrawerHeader>', () => {
+  beforeEach(() => {
+    cy.mount(VueDrawerHeader, {});
+  });
+
+  it('renders logo', () => {
+    cy.window().then(() => {
+      cy.dataCy('logo')
+      .should('be.visible')
+      .should('have.css', 'height', '40px');
+    });
+  });
+
+  it('renders help icon', () => {
+    cy.window().then(() => {
+      cy.dataCy('icon-help')
+        .should('be.visible')
+        .should('have.css', 'color', hexToRgb('#000000'))
+        .should('have.css', 'width', '24px')
+        .should('contain.text', 'help');
+    });
+  });
+
+  it('renders notifications icon', () => {
+    cy.window().then(() => {
+      cy.dataCy('icon-notification')
+        .should('be.visible')
+        .should('have.css', 'color', hexToRgb('#000000'))
+        .should('have.css', 'width', '24px')
+        .should('contain.text', 'notifications');
+    });
+  });
+});
