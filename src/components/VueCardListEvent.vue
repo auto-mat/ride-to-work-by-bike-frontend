47e576ec301f6931a4af48d89fa1de726f56c5a1
Šimon Macek <simon.macek@proficio.cz>
2023-06-10 15:02:08 +0200

    card-list-event test

---
+++ src/components/VueCardListEvent.vue
@@ -0,0 +1,33 @@
+<script lang="ts">
+import { defineComponent } from 'vue';
+import VueCardEvent from 'components/VueCardEvent.vue';
+
+import { CardEvent } from 'components/types';
+
+export default defineComponent({
+  name: 'VueCardListEvent',
+  props: {
+    heading: {
+      type: String,
+    },
+    cards: {
+      type: Array as () => CardEvent[],
+    },
+  },
+  components: {
+    VueCardEvent,
+  },
+});
+</script>
+
+<template>
+  <div>
+    <vue-card-event
+      v-for="card in cards"
+      :key="card.title"
+      :card="card"
+      class="q-mt-lg"
+      data-cy="card-list-item"
+    ></vue-card-event>
+  </div>
+</template>
