66ba06c2d43e40869ab1078159c5491ce700e16a
Šimon Macek <simon.macek@proficio.cz>
2023-06-12 21:36:17 +0200

    testing event countdown

--- src/components/__tests__/VueEventCountdown.cy.js
+++ src/components/__tests__/VueEventCountdown.cy.js
@@ -2,8 +2,7 @@ import VueEventCountdown from '../VueEventCountdown.vue';
 import { i18n } from '../../boot/i18n';
 
 describe('Event Countdown', () => {
-  const releaseDate = new Date('2023-10-01T12:00:00');
-  const currentTime = new Date('2023-09-30T12:00:00');
+  const releaseDate = '2023-10-01T12:00:00';
 
   beforeEach(() => {
     cy.mount(VueEventCountdown, {
@@ -20,7 +19,7 @@ describe('Event Countdown', () => {
       'index.countdown.hours',
       'index.countdown.minutes',
       'index.countdown.seconds',
-    ];
+    ]
 
     translationKeyList.forEach((translationKey) => {
       const defaultEnglishString = i18n.global.t(translationKey, 'en');
@@ -39,104 +38,26 @@ describe('Event Countdown', () => {
     });
   });
 
-  it('displays internationalized labels', () => {
-    cy.dataCy('countdown-label-days').should(
+  it('should render the component', () => {
+    cy.dataCy("countdown-component").should('exist');
+  });
+
+  it('should display internationalized labels', () => {
+    cy.dataCy("countdown-label-days").should(
       'contain.text',
-      i18n.global.t('index.countdown.days')
+      i18n.global.$t('index.countdown.days')
     );
-    cy.dataCy('countdown-label-hours').should(
+    cy.dataCy("countdown-label-hours").should(
       'contain.text',
-      i18n.global.t('index.countdown.hours')
+      i18n.global.$t('index.countdown.hours')
     );
-    cy.dataCy('countdown-label-minutes').should(
+    cy.dataCy("countdown-label-minutes").should(
       'contain.text',
-      i18n.global.t('index.countdown.minutes')
+      i18n.global.$t('index.countdown.minutes')
     );
-    cy.dataCy('countdown-label-seconds').should(
+    cy.dataCy("countdown-label-seconds").should(
       'contain.text',
-      i18n.global.t('index.countdown.seconds')
+      i18n.global.$t('index.countdown.seconds')
     );
-  });
-
-  it('renders a title with correct styles', () => {
-    cy.window().then(() => {
-      cy.dataCy('title')
-        .should('be.visible')
-        .should('have.css', 'font-size', '14px')
-        .should('have.css', 'font-weight', '700');
-    });
-  });
-
-  it('renders the date in correct format', () => {
-    cy.dataCy('title').should('be.visible').should('contain', '1. 10.');
-  });
-
-  it('renders the numbers in correct format', () => {
-    cy.dataCy('countdown-days')
-      .should('be.visible')
-      .should('have.css', 'font-size', '64px')
-      .should('have.css', 'font-weight', '700');
-    cy.dataCy('countdown-hours')
-      .should('be.visible')
-      .should('have.css', 'font-size', '64px')
-      .should('have.css', 'font-weight', '700');
-    cy.dataCy('countdown-minutes')
-      .should('be.visible')
-      .should('have.css', 'font-size', '64px')
-      .should('have.css', 'font-weight', '700');
-    cy.dataCy('countdown-seconds')
-      .should('be.visible')
-      .should('have.css', 'font-size', '64px')
-      .should('have.css', 'font-weight', '700');
-  });
-
-  it('renders the labels in correct format', () => {
-    cy.dataCy('countdown-label-days')
-      .should('be.visible')
-      .should('have.css', 'font-size', '14px')
-      .should('have.css', 'font-weight', '400');
-    cy.dataCy('countdown-label-hours')
-      .should('be.visible')
-      .should('have.css', 'font-size', '14px')
-      .should('have.css', 'font-weight', '400');
-    cy.dataCy('countdown-label-minutes')
-      .should('be.visible')
-      .should('have.css', 'font-size', '14px')
-      .should('have.css', 'font-weight', '400');
-    cy.dataCy('countdown-label-seconds')
-      .should('be.visible')
-      .should('have.css', 'font-size', '14px')
-      .should('have.css', 'font-weight', '400');
-  });
-
-  it('has gray background', () => {
-    cy.window().then(() => {
-      cy.dataCy('card')
-        .should('be.visible')
-        .should('have.css', 'background-color', 'rgb(243, 247, 255)');
-    });
-  });
-
-  it('counts down correctly', () => {
-    cy.clock(currentTime.getTime()).then(() => {
-      cy.dataCy('countdown-days').should('have.text', '1');
-      cy.dataCy('countdown-hours').should('have.text', '0');
-      cy.dataCy('countdown-minutes').should('have.text', '0');
-      cy.dataCy('countdown-seconds').should('have.text', '0');
-
-      cy.tick(1000);
-
-      cy.dataCy('countdown-days').should('have.text', '0');
-      cy.dataCy('countdown-hours').should('have.text', '23');
-      cy.dataCy('countdown-minutes').should('have.text', '59');
-      cy.dataCy('countdown-seconds').should('have.text', '59');
-
-      cy.tick(60 * 60 * 1000);
-
-      cy.dataCy('countdown-days').should('have.text', '0');
-      cy.dataCy('countdown-hours').should('have.text', '22');
-      cy.dataCy('countdown-minutes').should('have.text', '59');
-      cy.dataCy('countdown-seconds').should('have.text', '59');
-    });
   });
 });
