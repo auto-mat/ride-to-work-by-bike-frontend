d1e650b40c2764f58ce8a7e1d4f20c72c868151e
Šimon Macek <simon.macek@proficio.cz>
2023-06-14 17:00:01 +0200

    heading-background: tests + update

--- src/components/__tests__/VueHeadingBackground.cy.js
+++ src/components/__tests__/VueHeadingBackground.cy.js
@@ -18,8 +18,8 @@ describe('<VueHeadingBackground>', () => {
       cy.dataCy('heading')
         .should('be.visible')
         .should('contain', title)
-        .then(($title) => {
-          expect($title.text()).to.equal(title);
+        .then((titleNode) => {
+          expect(titleNode.text()).to.equal(title);
         });
 
       cy.dataCy('heading')
@@ -30,25 +30,35 @@ describe('<VueHeadingBackground>', () => {
 
   it('renders svg background', () => {
     cy.window().then(() => {
-      cy.dataCy('svg').should('be.visible');
+      cy.dataCy('svg').should('be.visible')
 
       cy.viewport('iphone-6');
 
-      cy.dataCy('svg')
-        .should('be.visible')
+      cy.dataCy('svg').should('be.visible')
         .then(($element) => {
-          expect($element.width()).to.be.closeTo(375, 0.5);
-          expect($element.height()).to.be.closeTo(73, 1);
+          expect($element.width()).to.be.closeTo(
+            375,
+            0.5
+          )
+          expect($element.height()).to.be.closeTo(
+            73,
+            1
+          )
         });
 
       cy.viewport('macbook-13');
 
-      cy.dataCy('svg')
-        .should('be.visible')
-        .then(($element) => {
-          expect($element.width()).to.be.closeTo(1280, 0.5);
-          expect($element.height()).to.be.closeTo(250, 1);
-        });
+      cy.dataCy('svg').should('be.visible')
+      .then(($element) => {
+        expect($element.width()).to.be.closeTo(
+          1280,
+          0.5
+        )
+        expect($element.height()).to.be.closeTo(
+          250,
+          1
+        )
+      });
     });
   });
 });
