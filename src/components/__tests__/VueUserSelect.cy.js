45bf94be5fc17c243ba3f7e9d05303726bc5234e
Šimon Macek <simon.macek@proficio.cz>
2023-06-14 21:30:58 +0200

    user select test dropdown

--- src/components/__tests__/VueUserSelect.cy.js
+++ src/components/__tests__/VueUserSelect.cy.js
@@ -1,25 +1,18 @@
 import VueUserSelect from '../VueUserSelect.vue';
 
-const options = [
-  { label: 'User 1', value: '1', image: 'https://picsum.photos/id/40/300/300' },
-  { label: 'User 2', value: '2', image: 'https://picsum.photos/id/64/300/300' },
-  { label: 'User 3', value: '3', image: 'https://picsum.photos/id/91/300/300' },
-];
+// TODO: add dynamic way of checking user data
+const image = "https://picsum.photos/id/40/300/300";
 
 describe('<VueUserSelect>', () => {
   beforeEach(() => {
-    cy.mount(VueUserSelect, {
-      props: {
-        options,
-      },
-    });
+    cy.mount(VueUserSelect, {});
   });
 
-  it('renders select with default value', () => {
-    cy.dataCy('user-select-input')
+  it('renders select', () => {
+    cy.dataCy('select')
       .should('be.visible')
       .should('have.css', 'height', '56px')
-      .should('contain', options[0].label);
+
   });
 
   it('renders rounded avatar', () => {
@@ -29,42 +22,16 @@ describe('<VueUserSelect>', () => {
       .find('img')
       .should(($img) => {
         expect($img[0].naturalWidth).to.be.greaterThan(0);
-        expect($img.attr('src')).to.equal(options[0].image);
+        expect($img.attr('src')).to.equal(image);
       });
-  });
+  })
 
-  it('shows dropdown on click', () => {
-    cy.dataCy('user-select-input')
-      .click()
-      .then(() => {
-        cy.get('.q-item__label')
-          .should('be.visible')
-          .should('have.length', 3)
-          .should('contain.text', 'User');
-      });
-  });
-
-  it('allows to change user', () => {
-    cy.dataCy('user-select-input')
-      .click()
-      .then(() => {
-        cy.get('.q-item__label')
-          .should('be.visible')
-          .should('have.length', 3)
-          .should('contain.text', 'User')
-          .last()
-          .click()
-          .then(() => {
-            cy.dataCy('avatar')
-              .should('be.visible')
-              .find('img')
-              .should(($img) => {
-                expect($img[0].naturalWidth).to.be.greaterThan(0);
-                expect($img.attr('src')).to.equal(options[2].image);
-              });
-
-            cy.dataCy('user-select-input').should('contain', options[2].label);
-          });
-      });
-  });
+  it ('shows dropdown on click', () => {
+    cy.dataCy('select').click().then(() => {
+      cy.get(".q-item__label")
+        .should('be.visible')
+        .should('have.length', 3)
+        .should('contain.text', 'User');
+    })
+  })
 });
