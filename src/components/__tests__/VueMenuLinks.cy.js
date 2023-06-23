79e576b2d514ca1a57ba2d5fbbb011bc085cb07e
Šimon Macek <simon.macek@proficio.cz>
2023-06-23 18:53:01 +0200

    vue menu links add test

--- src/components/__tests__/VueMenuLinks.cy.js
+++ src/components/__tests__/VueMenuLinks.cy.js
@@ -1,5 +1,4 @@
 import VueMenuLinks from 'components/VueMenuLinks.vue';
-import { i18n } from '../../boot/i18n';
 
 describe('<VueMenuLinks>', () => {
   const title = 'Find us on social media';
@@ -17,24 +16,9 @@ describe('<VueMenuLinks>', () => {
       .should('contain', title)
       .should('have.css', 'font-size', '24px')
       .should('have.css', 'font-weight', '700');
-  });
+  })
 
-  it('renders buttons in a grid', () => {
-    cy.mount(VueMenuLinks, {
-      props: {
-        title,
-        variant: 'social',
-      },
-    });
-
-    cy.dataCy('menu-links-list')
-      .should('be.visible')
-      .should('have.css', 'display', 'flex')
-      .should('have.css', 'flex-wrap', 'wrap')
-      .should('have.css', 'column-gap', '24px');
-  });
-
-  it('renders social buttons with correct styling (social variant)', () => {
+  it('renders social buttons with correct styling', () => {
     cy.mount(VueMenuLinks, {
       props: {
         title,
@@ -44,45 +28,10 @@ describe('<VueMenuLinks>', () => {
 
     cy.dataCy('button-menu-links')
       .should('have.length', 4)
-      .should('contain', i18n.global.t('index.menuLinks.instagram'))
-      // .should('have.attr', 'href', 'https://www.instagram.com/spolekautomat')
-      .should('contain', i18n.global.t('index.menuLinks.facebook'))
-      // .should('have.attr', 'href', 'https://www.facebook.com/spolekautomat')
-      .should('contain', i18n.global.t('index.menuLinks.twitter'))
-      // .should('have.attr', 'href', 'https://twitter.com/spolekautomat')
-      .should('contain', i18n.global.t('index.menuLinks.youtube'))
-      // .should('have.attr', 'href', 'https://www.youtube.com/@spolekautomat')
-      .should('have.backgroundColor', '#eceff1')
-      .should('have.css', 'border-radius', '28px')
-      .should('have.css', 'margin-top', '16px')
-      .find('.q-btn__content span')
-      .then(($el) => {
-        cy.wrap($el).should('have.color', '#000');
-      });
-  });
+      .should('contain', 'Instagram')
+      .should('contain', 'Facebook')
+      .should('contain', 'Twitter')
+      .should('contain', 'Youtube')
+    })
 
-  it('renders link buttons with correct styling (useful links variant)', () => {
-    cy.mount(VueMenuLinks, {
-      props: {
-        title,
-        variant: 'useful',
-      },
-    });
-
-    cy.dataCy('button-menu-links')
-      .should('have.length', 4)
-      .should('contain', 'Auto-Mat.cz')
-      .should('contain', 'Podpořte nás')
-      .should('contain', 'Kód projektu')
-      .should('contain', 'Mobilní aplikace')
-      .should('have.backgroundColor', '#eceff1')
-      .should('have.css', 'border-radius', '28px')
-      .should('have.css', 'margin-top', '16px')
-      .find('.q-btn__content span')
-      .then(($el) => {
-        cy.wrap($el).should('have.color', '#000');
-      });
-  });
-
-  // TODO: Check for icons
 });
