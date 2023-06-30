0b7c33ba67da5aea2822b7e0637e226f7b826c9c
Šimon Macek <simon.macek@proficio.cz>
2023-06-30 15:49:39 +0100

    e2e test: migrate desktop tests to mobile.

--- test/cypress/e2e/home.spec.cy.js
+++ test/cypress/e2e/home.spec.cy.js
@@ -62,8 +62,6 @@ describe('Home page', () => {
     });
 
     it('allows user to display help dialog and read all FAQ items', () => {
-      cy.viewport('macbook-13');
-
       cy.dataCy('link-help').last().should('be.visible').click();
 
       cy.dataCy('dialog-header').should('be.visible');
@@ -89,8 +87,6 @@ describe('Home page', () => {
     });
 
     it('allows user to display and submit contact form', () => {
-      cy.viewport('macbook-13');
-
       let i18n;
       cy.window().should('have.property', 'i18n');
       cy.window()
@@ -126,9 +122,7 @@ describe('Home page', () => {
         });
     });
 
-    it('allows validates contact form if there are errors', () => {
-      cy.viewport('macbook-13');
-
+    it('validates contact form if there are errors', () => {
       let i18n;
       cy.window().should('have.property', 'i18n');
       cy.window()
@@ -243,8 +237,6 @@ describe('Home page', () => {
     });
 
     it('allows user to show and hide left panel on mobile', () => {
-      cy.viewport('iphone-6');
-
       cy.dataCy('q-drawer').should('not.be.visible');
 
       cy.dataCy('drawer-open-button').click();
@@ -260,6 +252,180 @@ describe('Home page', () => {
 
       // cy.dataCy('q-drawer')
       //   .should('not.be.visible');
+    });
+
+    it('allows user to change profile to "User 3" and back to "User 1"', () => {
+      cy.dataCy('drawer-open-button').should('be.visible').click();
+
+      cy.dataCy('user-select').should('be.visible').click();
+
+      cy.get('.q-item__label')
+        .should('be.visible')
+        .should('have.length', 3)
+        .should('contain.text', 'User')
+        .last()
+        .click();
+
+      cy.dataCy('user-select-input').should('contain', 'User 3');
+
+      cy.dataCy('user-select-input').click();
+
+      cy.get('.q-item__label')
+        .should('be.visible')
+        .should('have.length', 3)
+        .should('contain.text', 'User')
+        .first()
+        .click();
+
+      cy.dataCy('user-select-input').should('contain', 'User 1');
+    });
+
+    it('allows user to display help dialog and read all FAQ items', () => {
+      cy.dataCy('drawer-open-button').should('be.visible').click();
+
+      cy.dataCy('link-help').last().should('be.visible').click();
+
+      cy.dataCy('dialog-header').should('be.visible');
+
+      cy.dataCy('list-faq-list')
+        .find('.q-card')
+        .each(($element) => {
+          cy.wrap($element).should('not.be.visible');
+        });
+
+      cy.dataCy('list-faq-list')
+        .find('.q-expansion-item')
+        .each(($element) => {
+          cy.wrap($element).should('be.visible');
+
+          cy.wrap($element).click();
+
+          cy.wrap($element)
+            .find('.q-card__section')
+            .should('be.visible')
+            .should('not.be.empty');
+        });
+    });
+
+    it('allows user to display and submit contact form', () => {
+      let i18n;
+      cy.window().should('have.property', 'i18n');
+      cy.window()
+        .then((win) => {
+          i18n = win.i18n;
+        })
+        .then(() => {
+          cy.dataCy('drawer-open-button').should('be.visible').click();
+
+          cy.dataCy('link-help').last().should('be.visible').click();
+
+          cy.dataCy('dialog-header').should('be.visible');
+
+          cy.dataCy('dialog-content').scrollTo(0, 1200);
+
+          cy.dataCy('button-contact').should('be.visible').click();
+
+          cy.dataCy('dialog-header').find('h3').should('be.visible');
+
+          cy.dataCy('contact-form-subject-input')
+            .should('be.visible')
+            .type('question');
+
+          cy.dataCy('contact-form-message-input')
+            .should('be.visible')
+            .type('what is the minimum distance to ride to work?');
+
+          cy.dataCy('contact-form-email-input')
+            .should('be.visible')
+            .type('P7LlQ@example.com');
+
+          cy.dataCy('contact-form-submit').should('be.visible').click();
+
+          // TODO: test successful submission
+        });
+    });
+
+    it('validates contact form if there are errors', () => {
+      let i18n;
+      cy.window().should('have.property', 'i18n');
+      cy.window()
+        .then((win) => {
+          i18n = win.i18n;
+        })
+        .then(() => {
+          cy.dataCy('drawer-open-button').should('be.visible').click();
+
+          cy.dataCy('link-help').last().should('be.visible').click();
+
+          cy.dataCy('dialog-header').should('be.visible');
+
+          cy.dataCy('dialog-content').scrollTo(0, 1200);
+
+          cy.dataCy('button-contact').should('be.visible').click();
+
+          cy.dataCy('dialog-header').find('h3').should('be.visible');
+
+          cy.dataCy('dialog-content').scrollTo('bottom');
+
+          cy.dataCy('contact-form-submit').should('be.visible').click();
+
+          cy.dataCy('dialog-content').scrollTo('top');
+
+          cy.dataCy('contact-form-subject')
+            .find('.q-field__messages')
+            .should('be.visible')
+            .should('contain', i18n.global.t('index.contact.subjectRequired'));
+
+          cy.dataCy('contact-form-subject')
+            .find('.q-field__control')
+            .should('have.class', 'text-negative');
+
+          cy.dataCy('dialog-content').scrollTo('top');
+
+          cy.dataCy('contact-form-subject-input')
+            .should('be.visible')
+            .type('question');
+
+          cy.dataCy('contact-form-subject')
+            .find('.q-field__messages')
+            .should('not.be.visible');
+
+          cy.dataCy('contact-form-subject')
+            .find('.q-field__control')
+            .should('not.have.class', 'text-negative');
+
+          cy.dataCy('dialog-content').scrollTo('bottom');
+
+          cy.dataCy('contact-form-submit').should('be.visible').click();
+
+          cy.dataCy('dialog-content').scrollTo('top');
+
+          cy.dataCy('contact-form-message')
+            .find('.q-field__messages')
+            .should('be.visible')
+            .should('contain', i18n.global.t('index.contact.messageRequired'));
+
+          cy.dataCy('contact-form-message')
+            .find('.q-field__control')
+            .should('have.class', 'text-negative');
+
+          cy.dataCy('contact-form-message-input')
+            .should('be.visible')
+            .type('what is the minimum distance to ride to work?');
+
+          cy.dataCy('dialog-content').scrollTo('bottom');
+
+          cy.dataCy('contact-form-submit').should('be.visible').click();
+
+          cy.dataCy('contact-form-email')
+            .find('.q-field__messages')
+            .should('be.visible')
+            .should('contain', i18n.global.t('index.contact.emailRequired'));
+
+          cy.dataCy('contact-form-email')
+            .find('.q-field__control')
+            .should('have.class', 'text-negative');
+        });
     });
   });
 
