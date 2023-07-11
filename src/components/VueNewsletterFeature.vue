b3e21cdb7ff4d9eb7bfa6c112fabf7682ec29848
Šimon Macek <simon.macek@proficio.cz>
2023-07-11 22:21:59 +0100

    newsletter component shell

---
+++ src/components/VueNewsletterFeature.vue
@@ -0,0 +1,13 @@
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: 'VueNewsletterFeature',
+  props: {}
+})
+</script>
+
+<template>
+  <div>
+  </div>
+</template>
