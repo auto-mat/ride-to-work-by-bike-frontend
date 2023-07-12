a7ef372c1550adf981569e32a081a7fb7cfc8566
Šimon Macek <simon.macek@proficio.cz>
2023-07-12 22:52:50 +0100

    add component newsletter tests

---
+++ src/components/VueNewsletterItem.vue
@@ -0,0 +1,39 @@
+<script lang="ts">
+import { defineComponent } from "vue";
+
+// import types
+import { NewsletterItem } from "components/types";
+
+export default defineComponent({
+  name: 'VueNewsletterItem',
+  props: {
+    item: {
+      type: Object as () => NewsletterItem,
+      required: true,
+    }
+  }
+})
+</script>
+
+<template>
+  <div class="flex items-center justify-between gap-8" data-cy="newsletter-feature-item">
+    <div>
+      <q-icon name="ion-bike" size="30px" color="black" data-cy="newsletter-feature-icon"></q-icon>
+      <div class="text-body" data-cy="newsletter-feature-title">{{ item.title }}</div>
+    </div>
+    <q-btn v-if="item.following" data-cy="newsletter-feature-button">
+      <q-icon name="ion-bike" size="30px" color="black"></q-icon>
+      {{ $t('index.newsletterFeatured.following') }}
+    </q-btn>
+    <q-btn v-if="!item.following">
+      <q-icon name="ion-bike" size="30px" color="black"></q-icon>
+      {{ $t('index.newsletterFeatured.follow') }}
+    </q-btn>
+  </div>
+</template>
+
+<style scoped lang="scss">
+.gap-8 {
+  gap: 8px;
+}
+</style>
--- src/components/__tests__/VueNewsletterFeature.cy.js
+++ src/components/__tests__/VueNewsletterFeature.cy.js
@@ -1,5 +1,8 @@
 import VueNewsletterFeature from 'components/VueNewsletterFeature.vue';
 
+const title = '';
+const description = '';
+
 describe('<VueNewsletterFeature>', () => {
   it('has translation for all strings', () => {
     cy.testLanguageStringsInContext(["title","subtitle","aboutChallenges","aboutEvents","aboutMobility","following","follow"], 'index.newsletterFeature');
@@ -11,6 +14,45 @@ describe('<VueNewsletterFeature>', () => {
         props: {},
       });
       cy.viewport('macbook-16');
+    });
+
+    it('renders title', () => {
+      cy.window().then(() => {
+        cy.dataCy('newsletter-feature-title')
+          .should('have.css', 'font-size', '20px')
+          .should('have.css', 'font-weight', '500')
+          .should('have.color', '#000000')
+          .should('contain', title)
+          .then(($title) => {
+            expect($title.text()).to.equal(title);
+          });
+      });
+    });
+
+    it('renders description', () => {
+      cy.window().then(() => {
+        cy.dataCy('newsletter-feature-description')
+          .should('have.css', 'font-size', '14px')
+          .should('have.css', 'font-weight', '500')
+          .should('have.color', '#000000')
+          .should('contain', description)
+          .then(($title) => {
+            expect($title.text()).to.equal(description);
+          });
+      });
+    });
+
+    it('renders description', () => {
+      cy.window().then(() => {
+        cy.dataCy('newsletter-feature-description')
+          .should('have.css', 'font-size', '14px')
+          .should('have.css', 'font-weight', '500')
+          .should('have.color', '#000000')
+          .should('contain', description)
+          .then(($title) => {
+            expect($title.text()).to.equal(description);
+          });
+      });
     });
   });
 
