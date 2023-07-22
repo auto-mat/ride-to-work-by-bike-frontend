47b550316108493b15ed2d1175ac1e49af4bc989
Šimon Macek <simon.macek@proficio.cz>
2023-07-22 17:57:07 +0200

    banner-image: update tests

--- src/components/__tests__/BannerImage.cy.js
+++ src/components/__tests__/BannerImage.cy.js
@@ -11,16 +11,20 @@ describe('<BannerImage>', () => {
     alt: 'road lined with trees',
   };
 
-  beforeEach(() => {
-    cy.mount(BannerImage, {
-      props: {
-        banner: {
-          title,
-          perex,
-          image,
-        },
-      },
+  context('desktop', () => {
+    beforeEach(() => {
+      cy.mount(BannerImage, {
+        props: {
+          banner: {
+            title,
+            perex,
+            image,
+          },
+        }
+      })
     });
+
+    cy.viewport('macbook-16');
 
     it('renders two columns', () => {
       cy.window().then(() => {
@@ -30,6 +34,8 @@ describe('<BannerImage>', () => {
           .should('have.css', 'flex-wrap', 'wrap');
 
         cy.dataCy('banner-half').should('have.length', 2).should('be.visible');
+
+        cy.testElementPercentageWidth(cy.dataCy('banner-half'), 50);
       });
     });
 
@@ -37,6 +43,9 @@ describe('<BannerImage>', () => {
       cy.window().then(() => {
         cy.dataCy('banner-title')
           .should('be.visible')
+          .should('have.css', 'font-size', '16px')
+          .should('have.css', 'font-weight', '500')
+          .should('have.color', '#000000')
           .should('contain.text', title)
           .then(($title) => {
             expect($title.text()).to.equal(title);
@@ -48,6 +57,9 @@ describe('<BannerImage>', () => {
       cy.window().then(() => {
         cy.dataCy('banner-perex')
           .should('be.visible')
+          .should('have.css', 'font-size', '12px')
+          .should('have.css', 'font-weight', '400')
+          .should('have.color', '#000000')
           .should('contain.text', perex)
           .then((perexNode) => {
             expect(perexNode.text()).to.equal(perex);
@@ -55,46 +67,137 @@ describe('<BannerImage>', () => {
       });
     });
 
-  it('renders image', () => {
-    cy.window().then(() => {
-      cy.dataCy('banner')
-        .find('img')
-        .should('be.visible')
-        .then(($img) => {
-          const naturalHeight = $img[0].naturalHeight;
-          expect(naturalHeight).to.be.greaterThan(0);
-          expect($img.attr('src')).to.equal(image.src);
+    it('renders image', () => {
+      cy.window().then(() => {
+        cy.dataCy('banner')
+          .find('img')
+          .should('be.visible')
+          .then(($img) => {
+            const naturalHeight = $img[0].naturalHeight;
+            expect(naturalHeight).to.be.greaterThan(0);
+            expect($img.attr('src')).to.equal(image);
+          });
+
+        cy.dataCy('banner').find('img').matchImageSnapshot({
+          failureThreshold: 0.5,
+          failureThresholdType: 'percent',
         });
+      });
     });
 
-  it('has correct background color', () => {
-    cy.window().then(() => {
-      cy.dataCy('banner')
-        .should('have.backgroundColor', config.colorGrayLight);
+    it('has correct background color', () => {
+      cy.window().then(() => {
+        cy.dataCy('banner')
+          .should('have.backgroundColor', config.colorGrayLight);
+      });
+
+    it('has rounded corners', () => {
+      cy.window().then(() => {
+        cy.dataCy('banner')
+          .should('be.visible')
+          .should('have.css', 'border-radius', '20px')
+          .should('have.css', 'overflow', 'hidden');
+
+          cy.dataCy('banner-half')
+            .first()
+            .should('have.css', 'border-top-left-radius', '20px')
+            .should('have.css', 'border-bottom-left-radius', '20px');
+        });
+      });
+    })
+  })
+
+  context('mobile', () => {
+    beforeEach(() => {
+      cy.mount(VueBannerImage, {
+        props: {
+          banner: {
+            title,
+            perex,
+            image,
+          },
+        },
+      });
+
+      cy.viewport('iphone-6');
     });
 
-  it('has rounded corners', () => {
-    cy.window().then(() => {
-      cy.dataCy('banner')
-        .should('be.visible')
-        .should('have.css', 'border-radius', '20px')
-        .should('have.css', 'overflow', 'hidden');
+    it('renders single column', () => {
+      cy.window().then(() => {
+        cy.dataCy('banner')
+          .should('be.visible')
+          .should('have.css', 'display', 'flex')
+          .should('have.css', 'flex-wrap', 'wrap');
 
-        cy.viewport('iphone-6');
+        cy.dataCy('banner-half').should('have.length', 2).should('be.visible');
+
+        cy.testElementPercentageWidth(cy.dataCy('banner-half'), 100);
+      });
+    });
+
+    it('renders title', () => {
+      cy.window().then(() => {
+        cy.dataCy('banner-title')
+          .should('be.visible')
+          .should('have.css', 'font-size', '16px')
+          .should('have.css', 'font-weight', '500')
+          .should('have.color', '#000000')
+          .should('contain.text', title)
+          .then(($title) => {
+            expect($title.text()).to.equal(title);
+          });
+      });
+    });
+
+    it('renders perex', () => {
+      cy.window().then(() => {
+        cy.dataCy('banner-perex')
+          .should('be.visible')
+          .should('have.css', 'font-size', '12px')
+          .should('have.css', 'font-weight', '400')
+          .should('have.color', '#000000')
+          .should('contain.text', perex)
+          .then((perexNode) => {
+            expect(perexNode.text()).to.equal(perex);
+          });
+      });
+    });
+
+    it('renders image', () => {
+      cy.window().then(() => {
+        cy.dataCy('banner')
+          .find('img')
+          .should('be.visible')
+          .then(($img) => {
+            const naturalHeight = $img[0].naturalHeight;
+            expect(naturalHeight).to.be.greaterThan(0);
+            expect($img.attr('src')).to.equal(image);
+          });
+
+        cy.dataCy('banner').find('img').matchImageSnapshot({
+          failureThreshold: 0.5,
+          failureThresholdType: 'percent',
+        });
+      });
+    });
+
+    it('has correct background color', () => {
+      cy.window().then(() => {
+        cy.dataCy('banner').should('have.backgroundColor', config.colorGrayLight);
+      });
+    });
+
+    it('has rounded corners', () => {
+      cy.window().then(() => {
+        cy.dataCy('banner')
+          .should('be.visible')
+          .should('have.css', 'border-radius', '20px');
 
         cy.dataCy('banner-half')
           .first()
           .should('have.css', 'border-top-left-radius', '20px')
           .should('have.css', 'border-top-right-radius', '20px');
-
-        cy.viewport('macbook-13');
-
-        cy.dataCy('banner-half')
-          .first()
-          .should('have.css', 'border-top-left-radius', '20px')
-          .should('have.css', 'border-bottom-left-radius', '20px');
       });
     });
   })
-})
 });
