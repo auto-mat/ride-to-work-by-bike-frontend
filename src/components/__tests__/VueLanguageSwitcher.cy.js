7cbda50c47a8eb1a01948c1917aded0ba8bdce9f
Šimon Macek <simon.macek@proficio.cz>
2023-08-04 13:41:12 +0200

    refactor: vue language switcher

---
+++ src/components/__tests__/VueLanguageSwitcher.cy.js
@@ -0,0 +1,80 @@
+import VueLanguageSwitcher from 'components/VueLanguageSwitcher.vue';
+import { i18n } from '../../../src/boot/i18n';
+
+describe('<VueLanguageSwitcher>', () => {
+  it('has translation for all strings', () => {
+    cy.testLanguageStringsInContext([], 'index.vueLanguageSwitcher');
+  });
+
+  context('desktop', () => {
+    beforeEach(() => {
+      cy.mount(VueLanguageSwitcher, {
+        props: {},
+      });
+      cy.viewport('macbook-16');
+    });
+
+    it('renders link for each locale', () => {
+      const locales = i18n.global.availableLocales;
+      locales.forEach((locale) => {
+        cy.dataCy('switcher-' + locale).should('exist').should('be.visible');
+      })
+    })
+
+    it('renders links with correct formatting', () => {
+      const locales = i18n.global.availableLocales;
+      locales.forEach((locale) => {
+        cy.dataCy('switcher-' + locale)
+          .should('have.css', 'font-size', '14px')
+          .should('have.css', 'font-weight', '400')
+          .find('a')
+          .should('have.css', 'text-decoration-line', 'none')
+          .should('have.color', '#fff');
+      })
+    })
+
+    it('highlights the active language', () => {
+      const activeLocale = i18n.global.locale;
+      cy.dataCy('switcher-' + activeLocale)
+        .find('a')
+        .should('be.visible')
+        .should('have.css', 'font-weight', '700');
+    })
+  });
+
+  context('mobile', () => {
+    beforeEach(() => {
+      cy.mount(VueLanguageSwitcher, {
+        props: {},
+      });
+      cy.viewport('iphone-6');
+    });
+
+    it('renders link for each locale', () => {
+      const locales = i18n.global.availableLocales;
+      locales.forEach((locale) => {
+        cy.dataCy('switcher-' + locale).should('exist').should('be.visible');
+      })
+    })
+
+    it('renders links with correct formatting', () => {
+      const locales = i18n.global.availableLocales;
+      locales.forEach((locale) => {
+        cy.dataCy('switcher-' + locale)
+          .should('have.css', 'font-size', '14px')
+          .should('have.css', 'font-weight', '400')
+          .find('a')
+          .should('have.css', 'text-decoration-line', 'none')
+          .should('have.color', '#fff');
+      })
+    })
+
+    it('highlights the active language', () => {
+      const activeLocale = i18n.global.locale;
+      cy.dataCy('switcher-' + activeLocale)
+        .should('be.visible')
+        .find('a')
+        .should('have.css', 'font-weight', '700');
+    })
+  });
+});
