c2d5d30713e3336f04a266f2f13e85e6f74a182b
Šimon Macek <simon.macek@proficio.cz>
2023-08-15 09:44:20 +0200

    refactor card types

---
+++ src/components/types/Card.ts
@@ -0,0 +1,68 @@
+export interface CardChallenge {
+  title: string;
+  image: string;
+  url: string;
+  dates: string;
+  company: boolean;
+}
+
+export interface CardEvent {
+  title: string;
+  thumbnail: string;
+  image: string;
+  dates: Date;
+  location: string;
+  content: string;
+  links: string[];
+}
+export interface CardFollow {
+  title: string;
+  handle: string;
+  image: string;
+  url: string;
+}
+export interface CardOffer {
+  title: string;
+  image: string;
+  validFrom: Date;
+  validTo: Date;
+  issuer: string;
+  content: string;
+  code: string;
+  url: string;
+}
+export interface CardPost {
+  title: string;
+  image: string;
+  date: Date;
+  url: string;
+}
+export interface CardProgress {
+  title: string;
+  icon: string;
+  percentage: number;
+  stats: [
+    {
+      title: string;
+      items: [
+        {
+          id: string;
+          text: string;
+        }
+      ]
+    },
+    {
+      title: string;
+      items: [
+        {
+          id: string;
+          text: string;
+        }
+      ]
+    }
+  ];
+  duration: {
+    current: number;
+    total: number;
+  };
+}
