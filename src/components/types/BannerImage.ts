84289fc4dba9cebe377b003f3e573c13068f8a89
Šimon Macek <simon.macek@proficio.cz>
2023-08-21 12:14:52 +0200

    add component: banner app

--- src/components/types/BannerImage.ts
+++
@@ -1,7 +0,0 @@
-import { Image } from './Image';
-
-export interface BannerImage {
-  title: string;
-  perex: string;
-  image: Image;
-}
--- src/components/types/index.ts
+++ src/components/types/index.ts
@@ -1,4 +1,4 @@
-export * from './BannerImage';
+export * from './Banner';
 export * from './Card';
 export * from './Countdown';
 export * from './Config';
