e8bbd4b687bd3797e493694808322d66b078d28f
Šimon Macek <simon.macek@proficio.cz>
2023-08-23 07:51:33 +0200

    format

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,32 +1,72 @@
 <template>
   <q-page data-cy="q-main">
     <div class="q-px-lg bg-white">
-      <h1 class="text-h5 q-mt-none q-pt-lg text-weight-bold" data-cy="index-title">
+      <h1
+        class="text-h5 q-mt-none q-pt-lg text-weight-bold"
+        data-cy="index-title"
+      >
         {{ $t('index.title') }}
       </h1>
-      <vue-event-countdown :release-date="releaseDate" data-cy="event-countdown"></vue-event-countdown>
+      <vue-event-countdown
+        :release-date="releaseDate"
+        data-cy="event-countdown"
+      ></vue-event-countdown>
       <vue-banner-app :banner="bannerApp"></vue-banner-app>
-      <vue-progress-slider :title="$t('index.progressSlider.title')" :cards="cardsProgressSlider" :stats="progressStats"
-        class="q-pt-xl q-pb-xl" :button="{ title: $t('index.progressSlider.button'), url: '/blog' }">
+      <vue-progress-slider
+        :title="$t('index.progressSlider.title')"
+        :cards="cardsProgressSlider"
+        :stats="progressStats"
+        class="q-pt-xl q-pb-xl"
+        :button="{ title: $t('index.progressSlider.button'), url: '/blog' }"
+      >
       </vue-progress-slider>
-      <vue-card-list-progress :title="$t('index.cardListProgress.title')" :cards="cardsProgress" :stats="progressStats"
-        class="q-pt-xl q-pb-xl" data-cy="list-progress">
+      <vue-card-list-progress
+        :title="$t('index.cardListProgress.title')"
+        :cards="cardsProgress"
+        :stats="progressStats"
+        class="q-pt-xl q-pb-xl"
+        data-cy="list-progress"
+      >
       </vue-card-list-progress>
-      <vue-card-list-challenge :cards="cardsChallenge" class="q-pt-xl q-pb-xl"
-        data-cy="list-challenge"></vue-card-list-challenge>
-      <vue-banner-image :banner="bannerImage" class="q-pt-xl q-pb-xl" data-cy="banner-image"></vue-banner-image>
+      <vue-card-list-challenge
+        :cards="cardsChallenge"
+        class="q-pt-xl q-pb-xl"
+        data-cy="list-challenge"
+      ></vue-card-list-challenge>
+      <vue-banner-image
+        :banner="bannerImage"
+        class="q-pt-xl q-pb-xl"
+        data-cy="banner-image"
+      ></vue-banner-image>
     </div>
-    <vue-heading-background :title="headingBgTitle" class="bg-white q-pt-xl"
-      data-cy="heading-background"></vue-heading-background>
+    <vue-heading-background
+      :title="headingBgTitle"
+      class="bg-white q-pt-xl"
+      data-cy="heading-background"
+    ></vue-heading-background>
     <div class="q-px-lg">
-      <vue-card-list-event :cards="cardsEvent" class="q-pt-xl" data-cy="list-event"></vue-card-list-event>
-      <vue-card-list-offer :title="$t('index.cardListOffer.title')" :cards="cardsOffer" class="q-pt-xl"
-        data-cy="list-offer">
+      <vue-card-list-event
+        :cards="cardsEvent"
+        class="q-pt-xl"
+        data-cy="list-event"
+      ></vue-card-list-event>
+      <vue-card-list-offer
+        :title="$t('index.cardListOffer.title')"
+        :cards="cardsOffer"
+        class="q-pt-xl"
+        data-cy="list-offer"
+      >
       </vue-card-list-offer>
-      <vue-card-list-post :title="$t('index.cardListPost.title')" :cards="cardsPost" :button="{
-        title: $t('index.cardListPost.button'),
-        url: '/blog',
-      }" class="q-pt-xl" data-cy="list-post">
+      <vue-card-list-post
+        :title="$t('index.cardListPost.title')"
+        :cards="cardsPost"
+        :button="{
+          title: $t('index.cardListPost.button'),
+          url: '/blog',
+        }"
+        class="q-pt-xl"
+        data-cy="list-post"
+      >
       </vue-card-list-post>
       <vue-newsletter-feature class="q-pt-xl" data-cy="newsletter-feature" />
       <vue-card-list-follow :cards="cardsFollow" class="q-pt-xl" />
