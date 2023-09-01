5b8ab899637299e8d54e1a41094e76ca6f6e4616
Šimon Macek <simon.macek@proficio.cz>
2023-09-01 11:24:47 +0200

    update alts with Image type

--- src/components/types/CardEvent.ts
+++ src/components/types/CardEvent.ts
@@ -1,7 +1,9 @@
+import { Image } from "./Image";
+
 export interface CardEvent {
   title: string;
-  thumbnail: string;
-  image: string;
+  thumbnail: Image;
+  image: Image;
   dates: Date;
   location: string;
   content: string;
