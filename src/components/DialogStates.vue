585524bd3c0f22ae464ecdce87f393f4a516a784
Šimon Macek <simon.macek@proficio.cz>
2023-09-12 15:03:49 +0200

    refactor dialog help

---
+++ src/components/DialogStates.vue
@@ -0,0 +1,145 @@
+<script lang="ts">
+import { defineComponent, ref, computed, Ref } from 'vue';
+
+// Component used to render dialog window which you can fill with custom content
+// Allows to switch between states
+// Available states:
+// - default
+// - form
+// Available slots:
+// - title
+// - content
+// Exposed v-slot props
+// - state
+// - setState
+// - reset
+// The content and image are rendered side by side in a scrollable dialog window
+export default defineComponent({
+  name: 'DialogStates',
+  props: {
+    modelValue: {
+      type: Boolean,
+      required: true,
+    },
+  },
+  emits: ['update:modelValue', 'change'],
+  setup(props, { emit }) {
+    const STATES = ['default', 'form'];
+
+    const activeState: Ref<'default' | 'form'> = ref('default');
+
+    const dialogOpen = computed({
+      get: () => props.modelValue,
+      set: (value) => {
+        emit('update:modelValue', value);
+        emit('change');
+      },
+    });
+
+    const setState = (value: 'form' | 'default') => {
+      activeState.value = value;
+    };
+
+    const reset = () => {
+      setState('default');
+      dialogOpen.value = false;
+    };
+
+    return {
+      STATES,
+      dialogOpen,
+      activeState,
+      setState,
+      reset,
+    };
+  },
+});
+</script>
+
+<template>
+  <q-dialog square persistent v-model="dialogOpen" data-cy="card-dialog">
+    <q-card class="full-width relative-position overflow-visible bg-white">
+      <!-- Dialog header -->
+      <q-card-section data-cy="dialog-header" class="row">
+        <!-- Navigation button: Default state (arrow) -->
+        <q-btn
+          v-if="activeState !== 'default'"
+          round
+          color="transparent"
+          size="xs"
+          unelevated
+          @click.prevent="activeState = 'default'"
+        >
+          <q-icon name="west" size="xs" color="black" />
+        </q-btn>
+        <!-- Title -->
+        <h3 class="text-h6 q-my-none">
+          <slot
+            name="title"
+            :state="activeState"
+            :setState="setState"
+            :reset="reset"
+          />
+        </h3>
+      </q-card-section>
+
+      <q-separator />
+
+      <!-- Dialog body -->
+      <q-card-section
+        class="scroll items-center q-pa-none"
+        data-cy="dialog-content"
+        style="max-height: 50vh; flex-wrap: wrap"
+      >
+        <div class="q-py-md">
+          <!-- Content for state Default -->
+          <slot
+            name="content"
+            :state="activeState"
+            :setState="setState"
+            :reset="reset"
+          ></slot>
+        </div>
+      </q-card-section>
+
+      <!-- Button close dialog -->
+      <q-card-actions
+        class="dialog-close inline-block absolute-top-right q-px-none q-py-none"
+        data-cy="dialog-close"
+      >
+        <q-btn
+          v-close-popup
+          round
+          unelevated
+          color="blue-grey-1"
+          icon="close"
+          class="text-blue-grey-10"
+        />
+      </q-card-actions>
+    </q-card>
+  </q-dialog>
+</template>
+
+<style lang="scss" scoped>
+.q-dialog__inner > div {
+  overflow: visible !important;
+}
+.gap-y-8 {
+  row-gap: 8px;
+}
+.gap-x-32 {
+  column-gap: 32px;
+}
+.dialog-close__wrapper {
+  top: -19px;
+  right: -19px;
+}
+.dialog-close {
+  width: 38px;
+  height: 38px;
+  min-width: 38px;
+  min-height: 38px;
+  top: -18px;
+  right: -18px;
+}
+</style>
--- src/components/DrawerHeader.vue
+++ src/components/DrawerHeader.vue
@@ -6,6 +6,7 @@ import MenuLinks from './MenuLinks.vue';
 import ListFaq from './ListFaq.vue';
 import ContactForm from './ContactForm.vue';
 import UserSelect from './UserSelect.vue';
+import DialogStates from './DialogStates.vue';
 
 export default defineComponent({
   name: 'DrawerHeader',
@@ -14,6 +15,7 @@ export default defineComponent({
     ListFaq,
     ContactForm,
     UserSelect,
+    DialogStates,
   },
   props: {
     showLogo: {
@@ -31,8 +33,7 @@ export default defineComponent({
   },
   emits: ['update:modelValue'],
   setup(props, { emit }) {
-    const dialogOpened = ref(false);
-    const dialogState = ref('default');
+    const isDialogOpen = ref(false);
 
     const classes = computed(() => {
       return props.showLogo ? 'justify-between' : 'justify-end';
@@ -45,30 +46,31 @@ export default defineComponent({
       },
     });
 
-    const resetDialog = () => {
-      dialogOpened.value = false;
-      dialogState.value = 'default';
-    };
-
     return {
       classes,
+      isDialogOpen,
       drawerOpened,
-      dialogOpened,
-      dialogState,
-      resetDialog,
     };
   },
 });
 </script>
 
 <template>
-  <div class="full-width flex items-center justify-between q-py-sm" :class="classes">
+  <div
+    class="full-width flex items-center justify-between q-py-sm"
+    :class="classes"
+  >
     <!-- RTWBB logo -->
-    <img class="logo" src="~assets/svg/logo.svg" :alt="$t('index.logoAltText')" data-cy="logo" />
+    <img
+      class="logo"
+      src="~assets/svg/logo.svg"
+      :alt="$t('index.logoAltText')"
+      data-cy="logo"
+    />
     <!-- Content -->
     <div class="flex items-center gap-24">
       <!-- Help icon link for displaying modal dialog-->
-      <a href="#" data-cy="link-help" @click.prevent="dialogOpened = true">
+      <a href="#" data-cy="link-help" @click.prevent="isDialogOpen = true">
         <q-icon name="help" size="sm" color="black" data-cy="icon-help" />
       </a>
       <!-- Notification icon link -->
@@ -84,63 +86,29 @@ export default defineComponent({
       <user-select variant="mobile" class="lt-md" />
     </div>
 
-    <!-- Modal dialog -->
-    <q-dialog
-      square
-      persistent
-      v-model="dialogOpened"
-      data-cy="dialog-help"
-      class="dialog-help"
-    >
-      <!-- Header -->
-      <q-card class="full-width relative-position overflow-visible bg-white">
-        <q-card-section
-          data-cy="dialog-header"
-          class="flex items-center gap-12"
-        >
-          <!-- Navigation button: Back (arrow) -->
-          <q-btn
-            v-if="dialogState !== 'default'"
-            round
-            color="transparent"
-            size="xs"
-            unelevated
-            @click.prevent="dialogState = 'default'"
-          >
-            <q-icon name="west" size="xs" color="black" />
-          </q-btn>
-          <!-- Dialog heading -->
-          <h3 class="text-h6 q-my-none">
-            <template v-if="dialogState === 'default'">
-              {{ $t('index.help.titleStateDefault') }}
-            </template>
-            <template v-if="dialogState === 'contact'">
-              {{ $t('index.help.titleStateContact') }}
-            </template>
-          </h3>
-        </q-card-section>
-
-        <q-separator />
-
-        <!-- Dialog content: Default state -->
-        <q-card-section
-          v-if="dialogState === 'default'"
-          class="scroll q-px-none"
-          data-cy="dialog-content"
-          style="max-height: 50vh"
-        >
+    <!-- Dialog -->
+    <dialog-states v-model="isDialogOpen">
+      <template #title="{ state }">
+        <span v-if="state === 'default'">
+          {{ $t('index.help.titleStateDefault') }}
+        </span>
+        <span v-if="state === 'form'">
+          {{ $t('index.help.titleStateContact') }}
+        </span>
+      </template>
+      <template #content="{ state, setState, reset }">
+        <div v-if="state === 'default'">
           <!-- FAQ for pariticipants -->
           <list-faq
             :title="$t('index.help.titleParticipants')"
             variant="participant"
-          ></list-faq>
+          />
           <!-- FAQ for Company coordinators -->
           <list-faq
             :title="$t('index.help.titleCoordinators')"
             variant="coordinator"
             class="q-mt-xl"
-          ></list-faq>
-
+          />
           <!-- Section: App Guide -->
           <div class="q-px-md q-mt-xl">
             <h4
@@ -160,7 +128,6 @@ export default defineComponent({
               data-cy="button-guide"
             />
           </div>
-
           <!-- Section: Contact us via form modal dialog -->
           <div class="q-px-md q-mt-xl">
             <h4
@@ -177,71 +144,27 @@ export default defineComponent({
               :label="$t('index.help.buttonContact')"
               class="q-mt-md"
               data-cy="button-contact"
-              @click.prevent="dialogState = 'contact'"
+              @click.prevent="setState('form')"
             />
           </div>
-
           <!-- Section: Useful links -->
-          <menu-links
-            :title="$t('index.help.titleLinks')"
-            variant="useful"
-          ></menu-links>
+          <menu-links :title="$t('index.help.titleLinks')" variant="useful" />
           <!-- Section: Social media links -->
-          <menu-links
-            :title="$t('index.help.titleSocials')"
-            variant="social"
-          ></menu-links>
-        </q-card-section>
-
-        <!-- Dialog content: Contact form state -->
-        <q-card-section
-          v-if="dialogState === 'contact'"
-          class="scroll"
-          style="max-height: 50vh"
-          data-cy="dialog-content"
-        >
-          <!-- Contact form -->
-          <contact-form @formSubmit="resetDialog"></contact-form>
-        </q-card-section>
-
-        <!-- Dialog: Button close -->
-        <q-card-actions
-          class="dialog-close inline-block absolute-top-right q-px-none q-py-none"
-          data-cy="dialog-close"
-        >
-          <q-btn
-            v-close-popup
-            round
-            unelevated
-            color="blue-grey-2"
-            icon="close"
-            class="text-blue-grey-9"
-          />
-        </q-card-actions>
-      </q-card>
-    </q-dialog>
+          <menu-links :title="$t('index.help.titleSocials')" variant="social" />
+        </div>
+        <div v-if="state === 'form'">
+          <contact-form @formSubmit="reset" class="q-px-md"></contact-form>
+        </div>
+      </template>
+    </dialog-states>
   </div>
 </template>
 
 <style scoped lang="scss">
-.gap-12 {
-  gap: 12px;
-}
-
-.gap-24 {
-  gap: 24px;
-}
-
 .logo {
   height: 40px;
 }
-
-.q-dialog__inner > div {
-  overflow: visible !important;
-}
-
-.dialog-close {
-  top: -21px;
-  right: -21px;
+.gap-24 {
+  gap: 24px;
 }
 </style>
