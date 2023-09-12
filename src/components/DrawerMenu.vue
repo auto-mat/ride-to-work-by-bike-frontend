97b9546b093a54f627a8f8399eea2b5f732ef75b
Šimon Macek <simon.macek@proficio.cz>
2023-09-12 15:34:26 +0200

    udpate drawer-menu

---
+++ src/components/DrawerMenu.vue
@@ -0,0 +1,64 @@
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+// mocks
+import { menuBottom, menuTop } from 'src/mocks/layout';
+
+export default defineComponent({
+  name: 'DrawerMenu',
+  setup() {
+    return {
+      menuTop,
+      menuBottom,
+    };
+  },
+});
+</script>
+
+<template>
+  <q-list>
+    <!-- Menu: Top items -->
+    <q-item
+      v-for="item in menuTop"
+      :key="item.name"
+      :to="item.url"
+      class="flex items-center gap-8"
+      clickable
+    >
+      <!-- Link icon -->
+      <q-icon :name="item.icon" size="xs" color="blue-grey-4" />
+      <!-- Link text -->
+      {{ $t(`drawerMenu.${item.name}`) }}
+    </q-item>
+
+    <q-separator color="blue-grey-2 q-my-sm" />
+
+    <!-- Menu: Bottom items -->
+    <q-item
+      v-for="item in menuBottom"
+      :key="item.name"
+      :to="item.url"
+      class="flex items-center gap-8"
+      clickable
+    >
+      <!-- Link icon -->
+      <q-icon :name="item.icon" size="xs" color="blue-grey-4" />
+      <!-- Link text -->
+      {{ $t(`drawerMenu.${item.name}`) }}
+    </q-item>
+  </q-list>
+</template>
+
+<style scoped lang="scss">
+.gap-8 {
+  gap: 8px;
+}
+
+.q-router-link--active {
+  color: $blue-grey-10 !important;
+}
+
+.q-router-link--active .q-icon {
+  color: $blue-grey-10 !important;
+}
+</style>
