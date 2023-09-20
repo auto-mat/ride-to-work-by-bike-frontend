52cdc37637e50683c43b1f4104ecc12dfe6c7568
Šimon Macek <simon.macek@proficio.cz>
2023-09-20 10:06:27 +0200

    list card offer: rename imports

--- src/components/ListCardOffer.vue
+++ src/components/ListCardOffer.vue
@@ -3,7 +3,7 @@
 import { defineComponent, computed } from 'vue';
 
 // components
-import VueCardOffer from './VueCardOffer.vue';
+import CardOffer from './CardOffer.vue';
 
 // types
 import { CardOffer as CardOfferType } from 'components/types';
@@ -11,7 +11,7 @@ import { CardOffer as CardOfferType } from 'components/types';
 export default defineComponent({
   name: 'ListCardOffer',
   components: {
-    VueCardOffer,
+    CardOffer,
   },
   props: {
     title: {
@@ -45,35 +45,21 @@ export default defineComponent({
 <template>
   <div>
     <!-- Title -->
-    <h2
-      class="text-h6 q-mt-none text-weight-semibold"
-      data-cy="card-list-post-title"
-    >
+    <h2 class="text-h6 q-mt-none text-weight-semibold" data-cy="card-list-post-title">
       {{ title }}
     </h2>
     <!-- Cards grid -->
     <div class="row q-col-gutter-lg q-row-gutter-md" data-cy="list-card-offer">
-      <div
-        v-for="card in renderedCards"
-        :key="card.title"
-        class="col-12 col-sm-6 col-lg-4"
-        data-cy="list-card-offer-item"
-      >
+      <div v-for="card in renderedCards" :key="card.title" class="col-12 col-sm-6 col-lg-4"
+        data-cy="list-card-offer-item">
         <!-- Card -->
-        <vue-card-offer :card="card" />
+        <card-offer :card="card" />
       </div>
     </div>
     <!-- Link more offers -->
     <div v-if="hasMoreCards" class="text-center">
-      <q-btn
-        rounded
-        color="black"
-        unelevated
-        outline
-        :label="$t('index.cardListOffer.button', { count: cards?.length })"
-        class="q-mt-md"
-        data-cy="list-card-offer-button"
-      />
+      <q-btn rounded color="black" unelevated outline :label="$t('index.cardListOffer.button', { count: cards?.length })"
+        class="q-mt-md" data-cy="list-card-offer-button" />
     </div>
   </div>
 </template>
