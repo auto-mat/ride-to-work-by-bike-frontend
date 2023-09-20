5f41e9915a6c00ae88d07960f782df4ec13a8212
Šimon Macek <simon.macek@proficio.cz>
2023-09-20 11:34:11 +0200

    card-event: update

--- src/components/CardEvent.vue
+++ src/components/CardEvent.vue
@@ -2,8 +2,8 @@
 import { defineComponent, ref } from 'vue';
 import { date, Screen } from 'quasar';
 
-// import components
-import VueDialogCard from 'components/VueDialogCard.vue';
+// components
+import DialogCard from 'components/DialogCard.vue';
 
 // import types
 import { CardEvent as CardEventType, ConfigGlobal } from 'components/types';
@@ -17,6 +17,9 @@ const { formatDate } = date;
 
 export default defineComponent({
   name: 'CardEvent',
+  components: {
+    DialogCard
+  },
   props: {
     card: {
       type: Object as () => CardEventType,
@@ -34,7 +37,6 @@ export default defineComponent({
     return {
       borderRadius,
       modalOpened,
-      modalDialog,
       eventDateTime,
       setHorizontalPosition,
     };
@@ -44,210 +46,104 @@ export default defineComponent({
 
 <template>
   <div>
-    <q-card
-      flat
-      class="bg-white"
-      :style="{ 'border-radius': borderRadius }"
-      data-cy="card"
-    >
-      <q-card-section
-        :horizontal="setHorizontalPosition()"
-        class="q-pa-none"
-        data-cy="card-section"
-      >
+    <q-card flat class="bg-white" :style="{ 'border-radius': borderRadius }" data-cy="card">
+      <q-card-section :horizontal="setHorizontalPosition()" class="q-pa-none" data-cy="card-section">
         <!-- Bg image -->
-        <q-img
-          :ratio="3 / 2"
-          :img-style="{
-            borderRadius: setHorizontalPosition()
-              ? `${borderRadius} 0 0 ${borderRadius}`
-              : `${borderRadius} ${borderRadius} 0 0`,
-          }"
-          class="col-sm-2"
-          :src="card?.thumbnail?.src"
-          :alt="card?.thumbnail?.alt"
-          data-cy="card-image"
-        />
+        <q-img :ratio="3 / 2" :img-style="{
+          borderRadius: setHorizontalPosition()
+            ? `${borderRadius} 0 0 ${borderRadius}`
+            : `${borderRadius} ${borderRadius} 0 0`,
+        }" class="col-sm-2" :src="card?.thumbnail?.src" :alt="card?.thumbnail?.alt" data-cy="card-image" />
         <!-- Content -->
-        <q-card-section
-          horizontal
-          class="col-grow flex items-center"
-          data-cy="card-content"
-        >
-          <div class="col-grow q-px-md q-py-lg">
+        <div class="col-grow flex wrap items-center q-py-lg" data-cy="card-content">
+          <div class="col-grow q-px-md">
             <!-- Event name link for open modal dialog -->
             <div class="text-subtitle1 text-bold" data-cy="card-title">
-              <a
-                href="#"
-                class="card-link text-dark block"
-                @click.prevent="modalOpened = true"
-                data-cy="card-link"
-              >
+              <a href="#" class="card-link text-dark block" @click.prevent="modalOpened = true" data-cy="card-link">
                 {{ card?.title }}
               </a>
             </div>
             <!-- Event date and place description -->
-            <div
-              v-if="eventDateTime || card?.location"
-              class="meta flex items-center gap-8 q-mt-sm"
-            >
-              <div
-                v-if="eventDateTime"
-                class="dates flex items-center"
-                data-cy="card-dates"
-              >
+            <div v-if="eventDateTime || card?.location" class="meta flex items-center gap-8 q-mt-sm">
+              <div v-if="eventDateTime" class="dates flex items-center" data-cy="card-dates">
                 <!-- Event calendar icon -->
-                <q-icon
-                  name="event"
-                  size="sm"
-                  class="q-pr-xs"
-                  color="blue-grey-2"
-                />
+                <q-icon name="event" size="sm" class="q-pr-xs" color="blue-grey-2" />
                 {{ eventDateTime }}
               </div>
-              <div
-                v-if="card?.location"
-                class="location flex items-center"
-                data-cy="card-location"
-              >
+              <div v-if="card?.location" class="location flex items-center" data-cy="card-location">
                 <!-- Event place icon -->
-                <q-icon
-                  name="place"
-                  size="sm"
-                  class="q-pr-xs"
-                  color="blue-grey-2"
-                />
+                <q-icon name="place" size="sm" class="q-pr-xs" color="blue-grey-2" />
                 {{ card?.location }}
               </div>
             </div>
           </div>
           <!-- Add to calendar btn -->
-          <div class="overflow-hidden flex items-center">
+          <div class="overflow-hidden flex items-center q-mt-md">
             <div class="q-px-md">
               <q-btn round outline data-cy="calendar-button">
                 <q-icon name="fa-solid fa-calendar-plus" size="xs"></q-icon>
               </q-btn>
             </div>
           </div>
-        </q-card-section>
+        </div>
       </q-card-section>
 
       <!-- Modal dialog -->
-      <q-dialog persistent v-model="modalOpened" square data-cy="card-dialog">
-        <q-card class="relative-position">
-          <!-- Header -->
-          <q-card-section class="q-pt-none" data-cy="dialog-header">
-            <h3 v-if="card?.title" class="text-h6 q-mt-sm q-pt-xs q-mb-none">
-              {{ card?.title }}
-            </h3>
-
-            <div
-              v-if="eventDateTime || card?.location"
-              class="meta flex items-center q-mt-sm"
-            >
-              <!-- Event datetime -->
-              <div
-                v-if="eventDateTime"
-                class="dates flex items-center q-pr-md"
-                data-cy="dialog-dates"
-              >
-                <!-- Event calendar icon -->
-                <q-icon
-                  name="event"
-                  size="sm"
-                  class="q-pr-xs"
-                  color="blue-grey-2"
-                />
-                {{ eventDateTime }}
-              </div>
-              <!-- Event location -->
-              <div
-                v-if="card?.location"
-                class="location flex items-center"
-                data-cy="dialog-location"
-              >
-                <!-- Event place icon -->
-                <q-icon
-                  name="place"
-                  size="sm"
-                  class="q-pr-xs"
-                  color="blue-grey-2"
-                />
-                {{ card?.location }}
+      <dialog-card v-model="modalOpened" data-cy="dialog-card-event">
+        <!-- Title -->
+        <template #title>
+          {{ card?.title }}
+        </template>
+        <!-- Metadata -->
+        <template v-if="eventDateTime || card?.location" #metadata>
+          <div class="flex flex-wrap items-center gap-x-32 gap-y-8 q-mt-sm">
+            <div v-if="eventDateTime" class="flex items-center text-blue-grey-7" data-cy="dialog-meta">
+              <q-icon name="event" size="18px" class="q-pr-xs" color="blue-grey-3" />
+              {{ eventDateTime }}
+            </div>
+            <div v-if="card?.location" class="flex items-center text-blue-grey-7" data-cy="dialog-meta">
+              <q-icon name="pedal_bike" size="18px" class="q-pr-xs" color="blue-grey-3" />
+              {{ card?.location }}
+            </div>
+          </div>
+        </template>
+        <!-- Content -->
+        <template v-if="card?.content" #content>
+          <div v-html="card.content" data-cy="dialog-content" />
+        </template>
+        <template #buttons>
+          <q-btn color="black" unelevated rounded class="q-mt-md" data-cy="dialog-button">
+            <div class="flex items-center no-wrap">
+              <q-icon left name="fa-solid fa-calendar-plus" size="xs" />
+              <div class="text-center">
+                {{ $t('index.cardEvent.addToCalendar') }}
               </div>
             </div>
-          </q-card-section>
-
-          <q-separator />
-
-          <!-- Content -->
-          <q-card-section
-            horizontal
-            class="scroll"
-            data-cy="dialog-body"
-            style="max-height: 50vh; flex-wrap: wrap"
-          >
-            <!-- Text -->
-            <div class="col-12 col-md-6 q-px-md q-py-md">
-              <div
-                v-if="card?.content"
-                v-html="card?.content"
-                data-cy="dialog-content"
-              ></div>
-            </div>
-
-            <!-- Image -->
-            <div class="col-12 col-md-6 q-px-md q-py-md">
-              <q-img
-                :src="card?.image?.src"
-                :alt="card?.image?.alt"
-                :ratio="1"
-                data-cy="dialog-image"
-              />
-            </div>
-
-            <!-- Add event to calendar btn -->
-            <q-btn
-              color="black"
-              unelevated
-              rounded
-              class="q-ma-md"
-              data-cy="dialog-button"
-            >
-              <div class="flex items-center no-wrap">
-                <q-icon left name="fa-solid fa-calendar-plus" size="xs" />
-                <div class="text-center">
-                  {{ $t('index.cardEvent.addToCalendar') }}
-                </div>
-              </div>
-            </q-btn>
-          </q-card-section>
-
-          <!-- Close dialog btn -->
-          <q-card-actions
-            class="dialog-close-btn inline-block absolute-top-right q-pa-none"
-            data-cy="dialog-close"
-          >
-            <q-btn
-              v-close-popup
-              round
-              color="blue-grey-8"
-              icon="close"
-              class="bg-blue-grey-2"
-            />
-          </q-card-actions>
-        </q-card>
-      </q-dialog>
+          </q-btn>
+        </template>
+        <!-- Image -->
+        <template v-if="card?.image" #image>
+          <q-img :src="card.image.src" :alt="card.image.alt" :ratio="1" data-cy="dialog-image" />
+        </template>
+      </dialog-card>
     </q-card>
   </div>
 </template>
 
 <style scoped lang="scss">
 .gap-8 {
-  gap: 8px;
+  row-gap: 8px;
 }
 
-.q-dialog__inner > div {
+.gap-y-8 {
+  row-gap: 8px;
+}
+
+.gap-x-32 {
+  column-gap: 32px;
+}
+
+.q-dialog__inner>div {
   overflow: visible !important;
 }
 
@@ -264,7 +160,7 @@ export default defineComponent({
   right: -21px;
 }
 
-.q-card > div:first-child > .q-img {
+.q-card>div:first-child>.q-img {
   border-top-left-radius: inherit;
   border-top-right-radius: inherit;
   border-bottom-left-radius: 0;
@@ -272,7 +168,7 @@ export default defineComponent({
 }
 
 @media (min-width: $breakpoint-sm-min) {
-  .q-card > div:first-child > .q-img {
+  .q-card>div:first-child>.q-img {
     border-top-left-radius: inherit;
     border-top-right-radius: 0;
     border-bottom-left-radius: inherit;
