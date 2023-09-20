d9d88aa4dce2eb9b617528e51a67cd26e12ba499
Šimon Macek <simon.macek@proficio.cz>
2023-09-20 11:48:47 +0200

    newsletter-item: fix responsivity

--- src/components/NewsletterItem.vue
+++ src/components/NewsletterItem.vue
@@ -26,50 +26,22 @@ export default defineComponent({
 </script>
 
 <template>
-  <div
-    class="flex items-center justify-end justify-sm-between gap-8"
-    data-cy="newsletter-item"
-  >
+  <div class="flex column row-sm gap-8 no-wrap items-sm-start justify-end justify-sm-between" data-cy="newsletter-item">
     <!-- Label section -->
-    <div
-      class="flex no-wrap items-center justify-between gap-8"
-      data-cy="newsletter-item-content"
-    >
+    <div class="flex no-wrap items-center gap-8" data-cy="newsletter-item-content">
       <!-- Icon -->
-      <q-icon
-        :name="item.icon"
-        size="32px"
-        color="blue-grey-6"
-        data-cy="newsletter-item-icon"
-      ></q-icon>
+      <q-icon :name="item.icon" size="32px" color="blue-grey-6" data-cy="newsletter-item-icon"></q-icon>
       <!-- Title -->
-      <div
-        class="text-body2 text-grey-10"
-        :class="{ 'text-bold': !item.following }"
-        data-cy="newsletter-item-title"
-      >
+      <div class="text-body2 text-grey-10" :class="{ 'text-bold': !item.following }" data-cy="newsletter-item-title">
         {{ item.title }}
       </div>
     </div>
 
     <!-- Button -->
-    <q-btn
-      rounded
-      color="grey-10"
-      :text-color="buttonTextColor"
-      unelevated
-      :outline="item.following"
-      data-cy="newsletter-item-button"
-      class="min-w-200"
-    >
+    <q-btn rounded color="grey-10" :text-color="buttonTextColor" unelevated :outline="item.following"
+      data-cy="newsletter-item-button" class="min-w-200">
       <!-- Icon -->
-      <q-icon
-        v-show="item.following"
-        name="check"
-        size="18px"
-        color="grey-10"
-        class="q-mr-xs"
-      />
+      <q-icon v-show="item.following" name="check" size="18px" color="grey-10" class="q-mr-xs" />
       <!-- Label -->
       <span v-if="item.following">
         {{ $t('index.newsletterFeature.following') }}
@@ -85,11 +57,25 @@ export default defineComponent({
 .gap-8 {
   gap: 8px;
 }
+
+.row-sm {
+  @media (min-width: $breakpoint-sm-min) {
+    flex-direction: row !important;
+  }
+}
+
 .justify-sm-between {
   @media (min-width: $breakpoint-sm-min) {
     justify-content: space-between;
   }
 }
+
+.items-sm-start {
+  @media (min-width: $breakpoint-sm-min) {
+    align-items: flex-start;
+  }
+}
+
 .min-w-200 {
   min-width: 200px;
 }
