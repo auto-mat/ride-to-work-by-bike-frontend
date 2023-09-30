f7cf64fdd1514c4969472b5a0d309a6d4d195311
Šimon Macek <simon.macek@proficio.cz>
2023-09-30 12:05:03 +0200

    newsletter-item: add function return type

--- src/components/NewsletterItem.vue
+++ src/components/NewsletterItem.vue
@@ -14,7 +14,7 @@ export default defineComponent({
     },
   },
   setup(props) {
-    const buttonTextColor = computed(() => {
+    const buttonTextColor = computed((): string => {
       return props.item.following ? 'grey-10' : 'white';
     });
 
