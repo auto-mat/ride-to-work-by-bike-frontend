3c488dd3bf94b217995c46a3f86914f7397ff42c
Šimon Macek <simon.macek@proficio.cz>
2023-09-30 15:07:25 +0200

    list-card-post: update swiper to element variant

--- src/components/ListCardPost.vue
+++ src/components/ListCardPost.vue
@@ -2,8 +2,7 @@
 // libraries
 import { defineComponent, computed } from 'vue';
 import { Screen } from 'quasar';
-import { Swiper, SwiperSlide } from 'swiper/vue';
-import { Navigation, A11y } from 'swiper/modules';
+import { register } from 'swiper/element/bundle';
 
 // components
 import CardPost from './CardPost.vue';
@@ -14,8 +13,6 @@ import { CardPost as CardPostType, Link } from './types';
 export default defineComponent({
   name: 'ListCardPost',
   components: {
-    Swiper,
-    SwiperSlide,
     CardPost,
   },
   props: {
@@ -33,6 +30,8 @@ export default defineComponent({
     },
   },
   setup() {
+    register();
+
     const isLargeScreen = computed((): boolean => {
       return Screen.gt.sm;
     });
@@ -43,7 +42,6 @@ export default defineComponent({
 
     return {
       buttonWidth,
-      modules: [Navigation, A11y],
     };
   },
 });
@@ -61,18 +59,17 @@ export default defineComponent({
       {{ title }}
     </h2>
     <!-- Swiper for news cards -->
-    <swiper
-      navigation
-      :modules="modules"
+    <swiper-container
+      :navigation="true"
       :slides-per-view="4"
       :space-between="24"
       :breakpoints="{
         0: {
-          slidesPerView: 1,
+          slidesPerView: 1.25,
           spaceBetween: 24,
         },
         600: {
-          slidesPerView: 2,
+          slidesPerView: 2.25,
         },
         1024: {
           slidesPerView: 3,
@@ -81,7 +78,7 @@ export default defineComponent({
           slidesPerView: 4,
         },
       }"
-      class="overflow-visible overflow-lg-hidden"
+      data-cy="swiper-container"
     >
       <swiper-slide
         v-for="(card, index) in cards"
@@ -90,11 +87,11 @@ export default defineComponent({
       >
         <card-post :card="card" data-cy="card-list-post-item" />
       </swiper-slide>
-    </swiper>
+    </swiper-container>
     <!-- Link to more news -->
     <div
       v-if="button"
-      class="text-center absolute-bottom"
+      class="text-center q-pt-md"
       data-cy="card-list-post-buttons"
     >
       <q-btn
@@ -117,89 +114,39 @@ export default defineComponent({
   z-index: 1;
 }
 
-// Display overflowing: next card indication (mobile)
-.swiper.overflow-visible {
-  overflow: visible;
-}
-
-// Default overflow to test as standalone (desktop)
-.swiper.overflow-lg-hidden {
-  @media (min-width: $breakpoint-lg-min) {
-    overflow: hidden;
-  }
-}
-
 // Styles for Swiper.js
-.swiper {
-  max-width: 90%;
-  margin-left: 0;
-  padding-bottom: 64px;
-
-  @media (min-width: $breakpoint-md-min) {
-    max-width: 100%;
-  }
+swiper-container::part(container) {
+  overflow-y: visible;
 }
 
-:deep(.swiper-button) {
-  position: absolute;
-  bottom: 0;
-  left: 0;
-  right: 0;
-  text-align: center;
-}
-
-:deep(.swiper-button-next),
-:deep(.swiper-button-prev) {
+swiper-container::part(button-prev),
+swiper-container::part(button-next) {
   border-radius: 9999px;
   top: auto;
-  bottom: 0;
+  bottom: -54px;
   width: 38px;
   height: 38px;
-  color: $grey-10;
+  color: transparent;
   background-color: #fff;
+  background-repeat: no-repeat;
+  background-position: center;
   border: 1px solid $grey-10;
-
   visibility: hidden;
 
   @media (min-width: $breakpoint-md-min) {
     visibility: visible;
   }
-
-  &:after {
-    width: 32px;
-    height: 32px;
-    content: '';
-  }
-
-  &.swiper-button-disabled {
-    opacity: 1;
-    border-color: $grey-5;
-
-    &:after {
-      opacity: 0.35;
-    }
-  }
 }
 
-:deep(.swiper-button-prev) {
+swiper-container::part(button-prev) {
   left: auto;
   right: 56px;
-
-  &:after {
-    background-image: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='32' height='32' viewBox='0 0 16 16'%3E%3Cpath fill='currentColor' fill-rule='evenodd' d='M12 8a.5.5 0 0 1-.5.5H5.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L5.707 7.5H11.5a.5.5 0 0 1 .5.5z'/%3E%3C/svg%3E");
-    background-repeat: no-repeat;
-    background-position: center;
-  }
+  background-image: url("../assets/svg/mdi-arrow-back.svg");
 }
 
-:deep(.swiper-button-next) {
+swiper-container::part(button-next) {
   left: auto;
   right: 0;
-
-  &:after {
-    background-image: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='32' height='32' viewBox='0 0 16 16'%3E%3Cpath fill='currentColor' fill-rule='evenodd' d='M4 8a.5.5 0 0 1 .5-.5h5.793L8.146 5.354a.5.5 0 1 1 .708-.708l3 3a.5.5 0 0 1 0 .708l-3 3a.5.5 0 0 1-.708-.708L10.293 8.5H4.5A.5.5 0 0 1 4 8z'/%3E%3C/svg%3E");
-    background-repeat: no-repeat;
-    background-position: center;
-  }
+  background-image: url("../assets/svg/mdi-arrow-forward.svg");
 }
 </style>
