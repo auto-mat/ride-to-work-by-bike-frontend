36be766a7d088dbc290819cc562f527f894bd545
Šimon Macek <simon.macek@proficio.cz>
2023-10-04 14:12:47 +0200

    card-offer: fix test

--- src/components/__tests__/CardOffer.cy.js
+++ src/components/__tests__/CardOffer.cy.js
@@ -201,7 +201,7 @@ describe('<CardOffer>', () => {
               .should('be.visible')
               .then(($img) => {
                 cy.testImageHeight($img);
-                expect($img.attr('src')).to.equal(card.image);
+                expect($img.attr('src')).to.equal(card.image.src);
               });
 
             cy.dataCy('dialog-body').find('img').matchImageSnapshot({
