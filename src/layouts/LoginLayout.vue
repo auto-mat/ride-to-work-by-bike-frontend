2df18b97dd4add6625f231bacc192c64b4c6ca3d
Šimon Macek <simon.macek@proficio.cz>
2023-10-15 20:57:28 +0200

    src/layouts: add i18n object to win for testing

--- src/layouts/LoginLayout.vue
+++ src/layouts/LoginLayout.vue
@@ -1,16 +1,22 @@
 <script lang="ts">
-import { defineComponent } from 'vue'
+// libraries
+import { defineComponent } from 'vue';
+import { i18n } from 'src/boot/i18n';
+
+// set global i18n object (for test purposes)
+if (window.Cypress) {
+  window.i18n = i18n;
+}
+
 export default defineComponent({
   name: 'LoginLayout',
-})
+});
 </script>
 
 <template>
   <q-layout view="hHh lpR fFf">
-
     <q-page-container>
       <router-view />
     </q-page-container>
-
   </q-layout>
 </template>
