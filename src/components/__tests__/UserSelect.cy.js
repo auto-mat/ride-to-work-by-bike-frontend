f51f37c22d858917084c4d84054e7f97ffe54cc8
Šimon Macek <simon.macek@proficio.cz>
2023-10-17 08:32:09 +0200

    UserSelect: split command chaining

--- src/components/__tests__/UserSelect.cy.js
+++ src/components/__tests__/UserSelect.cy.js
@@ -42,13 +42,11 @@ describe('<UserSelect>', () => {
     });
 
     it('shows dropdown on click', () => {
-      cy.dataCy('user-select-input')
-        .click()
-        .then(() => {
-          cy.get('.q-item__label')
-            .should('be.visible')
-            .should('have.length', 6);
-        });
+      cy.dataCy('user-select-input').click()
+
+      cy.get('.q-item__label')
+        .should('be.visible')
+        .should('have.length', 6);
     });
   });
 
