8a8c4fff3555c3fe21a5fa1a6f39889424ff27d0
Šimon Macek <simon.macek@proficio.cz>
2023-10-19 09:34:39 +0200

    LoginPage: add documentation

--- src/pages/LoginPage.vue
+++ src/pages/LoginPage.vue
@@ -1,28 +1,24 @@
-<template>
-  <q-page padding>
-    <div class="q-px-lg">
-      <!-- Page header -->
-      <div class="flex items-center justify-between q-py-lg">
-        <!-- RTWBB logo -->
-        <img
-          class="logo"
-          src="~assets/svg/logo.svg"
-          :alt="$t('index.logoAltText')"
-          :title="$t('header.siteTitle')"
-          data-cy="logo"
-        />
-        <div class="flex items-center gap-32">
-          <!-- Help icon link for displaying modal dialog -->
-          <help-button size="13px" color="primary" />
-          <!-- Language switcher -->
-          <language-switcher variant="light" />
-        </div>
-      </div>
-    </div>
-  </q-page>
-</template>
-
 <script lang="ts">
+/**
+ * LoginPage Component
+ *
+ * The `LoginPage` renders the page with login form and password reset form.
+ *
+ * @description
+ * This component is used to allow user to login into their account. Or
+ * reset their password. It allows login via Google and Facebook accounts.
+ * It contains links to App stores.
+ *
+ * @components
+ * - `HelpButton`: Component to render help icon with modal dialog.
+ * - `LanguageSwitcher`: Component that allows user to change language.
+ *
+ * @template
+ * - `LoginLayout`: Displayed within the `LoginLayout` template.
+ *
+ * @see [Figma Design](https://www.figma.com/file/L8dVREySVXxh3X12TcFDdR/Do-pr%C3%A1ce-na-kole?type=design&node-id=6269%3A24768&mode=dev)
+ */
+
 // libraries
 import { setCssVar } from 'quasar';
 import { defineComponent } from 'vue';
@@ -48,6 +44,30 @@ export default defineComponent({
   },
 });
 </script>
+
+<template>
+  <q-page padding>
+    <div class="q-px-lg">
+      <!-- Page header -->
+      <div class="flex items-center justify-between q-py-lg">
+        <!-- RTWBB logo -->
+        <img
+          class="logo"
+          src="~assets/svg/logo.svg"
+          :alt="$t('index.logoAltText')"
+          :title="$t('header.siteTitle')"
+          data-cy="logo"
+        />
+        <div class="flex items-center gap-32">
+          <!-- Help icon link for displaying modal dialog -->
+          <help-button size="13px" color="primary" />
+          <!-- Language switcher -->
+          <language-switcher variant="light" />
+        </div>
+      </div>
+    </div>
+  </q-page>
+</template>
 
 <style scoped lang="scss">
 .logo {
