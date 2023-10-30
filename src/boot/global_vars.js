250f38842d99240121e03e15f20c459d439cb63a
Šimon Macek <simon.macek@proficio.cz>
2023-10-30 21:04:38 +0100

    src/boot: add global_vars

---
+++ src/boot/global_vars.js
@@ -0,0 +1,20 @@
+import { setCssVar } from 'quasar';
+import { boot } from 'quasar/wrappers';
+
+// config
+const rideToWorkByBikeConfig = JSON.parse(
+  process.env.RIDE_TO_WORK_BY_BIKE_CONFIG,
+);
+
+const initVars = () => {
+  setCssVar('white', rideToWorkByBikeConfig.colorWhite);
+  setCssVar('black', rideToWorkByBikeConfig.colorBlack);
+  setCssVar('primary', rideToWorkByBikeConfig.colorPrimary);
+  setCssVar('secondary', rideToWorkByBikeConfig.colorSecondary);
+};
+
+export { rideToWorkByBikeConfig, initVars };
+
+export default boot(() => {
+  initVars();
+});
