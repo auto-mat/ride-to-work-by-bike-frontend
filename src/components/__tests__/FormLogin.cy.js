b846815281d2f0073a8e511d45a35d34e1cc1451
Šimon Macek <simon.macek@proficio.cz>
2023-12-11 21:32:39 -0500

    src/components: use email component in form login

--- src/components/__tests__/FormLogin.cy.js
+++ src/components/__tests__/FormLogin.cy.js
@@ -52,11 +52,7 @@ describe('<FormLogin>', () => {
     });
 
     it('renders email field', () => {
-      cy.dataCy('form-login-email')
-        .should('be.visible')
-        .find('label[for="form-login-email"]')
-        .should('be.visible')
-        .and('have.text', i18n.global.t('login.form.labelEmail'));
+      cy.dataCy('form-login-email').should('be.visible');
       cy.dataCy('form-login-email')
         .find('.q-field__control')
         .should('be.visible')
@@ -230,20 +226,9 @@ describe('<FormLogin>', () => {
 
     it('validates login form user inputs', () => {
       cy.dataCy('form-login-submit-login').should('be.visible').click();
-      // validate email required
-      cy.dataCy('form-login-email')
-        .find('.q-field__messages')
-        .should('be.visible')
-        .and('contain', i18n.global.t('login.form.messageEmailReqired'));
-      // validate email format
-      cy.dataCy('form-login-email-input').type('qw123@qw');
-      cy.dataCy('form-login-email')
-        .find('.q-field__messages')
-        .should('be.visible')
-        .and('contain', i18n.global.t('login.form.messageEmailInvalid'));
       // validate password required
       // first make email pass
-      cy.dataCy('form-login-email-input').type('.com');
+      cy.dataCy('form-login-email').find('input').type('test@example.com');
       cy.dataCy('form-login-submit-login').should('be.visible').click();
       cy.dataCy('form-login-password')
         .find('.q-field__messages')
@@ -268,142 +253,6 @@ describe('<FormLogin>', () => {
         .find('.q-field__messages')
         .should('be.visible')
         .and('contain', i18n.global.t('login.form.messageEmailInvalid'));
-    });
-
-    it('validates emails correctly', () => {
-      // invalid email abc.exam-ple.com
-      cy.dataCy('form-login-email-input').type('abc.example.com');
-      cy.dataCy('form-login-submit-login').should('be.visible').click();
-      cy.dataCy('form-login-email')
-        .find('.q-field__messages')
-        .and('contain', i18n.global.t('login.form.messageEmailInvalid'));
-      cy.dataCy('form-login-email-input').clear();
-      // invalid email a@b@c@example.com
-      cy.dataCy('form-login-email-input').type('a@b@c@example.com');
-      cy.dataCy('form-login-submit-login').should('be.visible').click();
-      cy.dataCy('form-login-email')
-        .find('.q-field__messages')
-        .and('contain', i18n.global.t('login.form.messageEmailInvalid'));
-      cy.dataCy('form-login-email-input').clear();
-      // invalid email
-      cy.dataCy('form-login-email-input').type(
-        'a"b(c)d,e:f;g<h>i[jk]l@example.com',
-      );
-      cy.dataCy('form-login-submit-login').should('be.visible').click();
-      cy.dataCy('form-login-email')
-        .find('.q-field__messages')
-        .and('contain', i18n.global.t('login.form.messageEmailInvalid'));
-      cy.dataCy('form-login-email-input').clear();
-      // invalid email
-      cy.dataCy('form-login-email-input').type('just"not"right@example.com');
-      cy.dataCy('form-login-submit-login').should('be.visible').click();
-      cy.dataCy('form-login-email')
-        .find('.q-field__messages')
-        .and('contain', i18n.global.t('login.form.messageEmailInvalid'));
-      cy.dataCy('form-login-email-input').clear();
-      // invalid email
-      cy.dataCy('form-login-email-input').type(
-        'this is"notallowed@example.com',
-      );
-      cy.dataCy('form-login-submit-login').should('be.visible').click();
-      cy.dataCy('form-login-email')
-        .find('.q-field__messages')
-        .and('contain', i18n.global.t('login.form.messageEmailInvalid'));
-      cy.dataCy('form-login-email-input').clear();
-      // invalid email
-      cy.dataCy('form-login-email-input').type(
-        'this still"not\\allowed@example.com',
-      );
-      cy.dataCy('form-login-submit-login').should('be.visible').click();
-      cy.dataCy('form-login-email')
-        .find('.q-field__messages')
-        .and('contain', i18n.global.t('login.form.messageEmailInvalid'));
-      cy.dataCy('form-login-email-input').clear();
-      // invalid email
-      cy.dataCy('form-login-email-input').type(
-        '1234567890123456789012345678901234567890123456789012345678901234+x@example.com',
-      );
-      cy.dataCy('form-login-submit-login').should('be.visible').click();
-      cy.dataCy('form-login-email')
-        .find('.q-field__messages')
-        .and('contain', i18n.global.t('login.form.messageEmailInvalid'));
-      cy.dataCy('form-login-email-input').clear();
-      // invalid email
-      cy.dataCy('form-login-email-input').type(
-        'i.like.underscores@but_they_are_not_allowed_in_this_part',
-      );
-      cy.dataCy('form-login-submit-login').should('be.visible').click();
-      cy.dataCy('form-login-email')
-        .find('.q-field__messages')
-        .and('contain', i18n.global.t('login.form.messageEmailInvalid'));
-      cy.dataCy('form-login-email-input').clear();
-      // valid email
-      cy.dataCy('form-login-email-input').type('simple@example.com');
-      cy.dataCy('form-login-submit-login').should('be.visible').click();
-      cy.get(
-        '*[data-cy="form-login-email"] .q-field__messages [role="alert"]',
-      ).should('not.exist');
-      cy.dataCy('form-login-email-input').clear();
-      // valid email
-      cy.dataCy('form-login-email-input').type('very.common@example.com');
-      cy.dataCy('form-login-submit-login').should('be.visible').click();
-      cy.get(
-        '*[data-cy="form-login-email"] .q-field__messages [role="alert"]',
-      ).should('not.exist');
-      cy.dataCy('form-login-email-input').clear();
-      // valid email
-      cy.dataCy('form-login-email-input').type('x@example.com');
-      cy.dataCy('form-login-submit-login').should('be.visible').click();
-      cy.get(
-        '*[data-cy="form-login-email"] .q-field__messages [role="alert"]',
-      ).should('not.exist');
-      cy.dataCy('form-login-email-input').clear();
-      // valid email
-      cy.dataCy('form-login-email-input').type(
-        'long.email-address-with-hyphens@and.subdomains.example.com',
-      );
-      cy.dataCy('form-login-submit-login').should('be.visible').click();
-      cy.get(
-        '*[data-cy="form-login-email"] .q-field__messages [role="alert"]',
-      ).should('not.exist');
-      cy.dataCy('form-login-email-input').clear();
-      // valid email
-      cy.dataCy('form-login-email-input').type(
-        'user.name+tag+sorting@example.com',
-      );
-      cy.dataCy('form-login-submit-login').should('be.visible').click();
-      cy.get(
-        '*[data-cy="form-login-email"] .q-field__messages [role="alert"]',
-      ).should('not.exist');
-      cy.dataCy('form-login-email-input').clear();
-      // valid email
-      cy.dataCy('form-login-email-input').type('name/surname@example.com');
-      cy.dataCy('form-login-submit-login').should('be.visible').click();
-      cy.get(
-        '*[data-cy="form-login-email"] .q-field__messages [role="alert"]',
-      ).should('not.exist');
-      cy.dataCy('form-login-email-input').clear();
-      // valid email
-      cy.dataCy('form-login-email-input').type('mailhost!username@example.org');
-      cy.dataCy('form-login-submit-login').should('be.visible').click();
-      cy.get(
-        '*[data-cy="form-login-email"] .q-field__messages [role="alert"]',
-      ).should('not.exist');
-      cy.dataCy('form-login-email-input').clear();
-      // valid email
-      cy.dataCy('form-login-email-input').type('user%example.com@example.org');
-      cy.dataCy('form-login-submit-login').should('be.visible').click();
-      cy.get(
-        '*[data-cy="form-login-email"] .q-field__messages [role="alert"]',
-      ).should('not.exist');
-      cy.dataCy('form-login-email-input').clear();
-      // valid email
-      cy.dataCy('form-login-email-input').type('user-@example.org');
-      cy.dataCy('form-login-submit-login').should('be.visible').click();
-      cy.get(
-        '*[data-cy="form-login-email"] .q-field__messages [role="alert"]',
-      ).should('not.exist');
-      cy.dataCy('form-login-email-input').clear();
     });
   });
 
