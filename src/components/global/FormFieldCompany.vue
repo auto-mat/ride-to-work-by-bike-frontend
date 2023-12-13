35da9b02c41996fb71a8059231243ccbe4f76328
Šimon Macek <simon.macek@proficio.cz>
2023-12-12 20:58:04 -0500

    components/global: add FormFieldCompany component

---
+++ src/components/global/FormFieldCompany.vue
@@ -0,0 +1,68 @@
+<script lang="ts">
+/**
+ * FormFieldCompany Component
+ *
+ * The `FormFieldCompany` renders company select
+ *
+ * @description * Use this component to allow user to select their company
+ * and create a new company to register under.
+ *
+ * Note: This component is commonly used in `FormRegisterCoordinator`.
+ *
+ * @props
+ * - `value` (string, required): The object representing user input.
+ *   It should be of type `string`.
+ *
+ * @events
+ * - `update:modelValue`: Emitted as a part of v-model structure.
+ *
+ * @example
+ * <form-field-company />
+ *
+ * @see [Figma Design](https://www.figma.com/file/L8dVREySVXxh3X12TcFDdR/Do-pr%C3%A1ce-na-kole?type=design&node-id=6356%3A25476&mode=dev)
+ */
+
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'FormFieldCompany',
+});
+</script>
+
+<template>
+  <div data-cy="form-company">
+    <!-- Label -->
+    <label for="form-company" class="text-caption text-bold">
+      {{ $t('register.coordinator.form.labelCompany') }}
+    </label>
+    <!-- Input -->
+    <q-select
+      dense
+      outlined
+      use-input
+      v-model="formRegisterCoordinator.company"
+      :options="options"
+      lazy-rules
+      :rules="[
+        (val) =>
+          isFilled(val) ||
+          $t('register.coordinator.form.messageFieldRequired', {
+            fieldName: $t('register.coordinator.form.labelCompanyShort'),
+          }),
+      ]"
+      @filter="onFilter"
+      class="q-mt-sm"
+      id="form-company"
+      name="company"
+      data-cy="form-company-input"
+    >
+      <template v-slot:no-option>
+        <q-item>
+          <q-item-section class="text-grey">
+            {{ $t('register.coordinator.form.messageNoCompany') }}
+          </q-item-section>
+        </q-item>
+      </template>
+    </q-select>
+  </div>
+</template>
