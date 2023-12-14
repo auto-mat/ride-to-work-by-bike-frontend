4831dc83f88979789ed50b306c6ba8a7f3400889
Šimon Macek <simon.macek@proficio.cz>
2023-12-14 11:55:25 -0500

    components/__tests__: add core field company tests

---
+++ src/components/__tests__/FormFieldCompany.cy.js
@@ -0,0 +1,47 @@
+import FormFieldCompanyTest from 'components/global/FormFieldCompanyTest.vue';
+import { i18n } from '../../boot/i18n';
+
+describe('<FormFieldCompany>', () => {
+  it('has translation for all strings', () => {
+    cy.testLanguageStringsInContext([], 'index.component', i18n);
+  });
+
+  context('desktop', () => {
+    beforeEach(() => {
+      cy.mount(FormFieldCompanyTest, {
+        props: {},
+      });
+      cy.viewport('macbook-16');
+    });
+
+    it('renders input with label', () => {
+      // input wrapper
+      cy.dataCy('form-company')
+        .find('.q-field__control')
+        .should('be.visible')
+        .and('have.css', 'border-radius', '8px');
+      // input label
+      cy.dataCy('form-company')
+        .find('label')
+        .should('be.visible')
+        .and('contain', i18n.global.t('form.labelCompany'));
+      // input
+      cy.dataCy('form-company').find('input').should('be.visible');
+    });
+
+    it('allows user to select option', () => {
+      cy.dataCy('form-company').find('input').click();
+      // select option
+      cy.get('.q-menu')
+        .should('be.visible')
+        .within(() => {
+          cy.get('.q-item').first().click();
+        });
+      // test selected option
+      cy.dataCy('form-company')
+        .find('input')
+        .invoke('val')
+        .should('eq', 'Company 1');
+    });
+  });
+});
