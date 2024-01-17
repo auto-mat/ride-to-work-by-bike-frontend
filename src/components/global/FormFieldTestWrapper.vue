ef137ca6a6dab86cde206f77b36f1197d36e38f4
Šimon Macek <simon.macek@proficio.cz>
2024-01-16 22:56:19 -0500

    __tests__: use wrapper for testing text required

--- src/components/global/FormFieldTestWrapper.vue
+++ src/components/global/FormFieldTestWrapper.vue
@@ -25,12 +25,14 @@ import { defineComponent, ref } from 'vue';
 // components
 import FormFieldPassword from './FormFieldPassword.vue';
 import FormFieldPasswordConfirm from './FormFieldPasswordConfirm.vue';
+import FormFieldTextRequired from './FormFieldTextRequired.vue';
 
 export default defineComponent({
   name: 'FormFieldTestWrapper',
   components: {
     FormFieldPassword,
     FormFieldPasswordConfirm,
+    FormFieldTextRequired,
   },
   props: {
     component: {
@@ -39,6 +41,15 @@ export default defineComponent({
     },
     compareValue: {
       type: String,
+    },
+    name: {
+      type: String,
+    },
+    label: {
+      type: String,
+    },
+    testing: {
+      type: Boolean,
     },
   },
   setup() {
@@ -55,6 +66,8 @@ export default defineComponent({
   <component
     :is="component"
     v-model="inputValue"
+    :name="name"
+    :label="label"
     :compare-value="compareValue"
   />
 </template>
