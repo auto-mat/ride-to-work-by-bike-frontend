9e5e59f4f268c6da19b8ac58a55da801795987f1
Šimon Macek <simon.macek@proficio.cz>
2024-01-20 21:01:51 -0500

    __tests__: add option group tests

--- src/components/__tests__/FormFieldOptionGroup.cy.js
+++ src/components/__tests__/FormFieldOptionGroup.cy.js
@@ -1,5 +1,14 @@
+import { colors } from 'quasar';
 import FormFieldTestWrapper from 'components/global/FormFieldTestWrapper.vue';
 import { i18n } from '../../boot/i18n';
+import { rideToWorkByBikeConfig } from '../../boot/global_vars';
+import { hexToRgb } from 'app/test/cypress/utils';
+
+const { getPaletteColor } = colors;
+const white = getPaletteColor('white');
+
+const borderRadius = rideToWorkByBikeConfig.borderRadiusCard;
+const colorPrimary = rideToWorkByBikeConfig.colorPrimary;
 
 describe('<FormFieldOptionGroup>', () => {
   it('has translation for all strings', () => {
@@ -18,6 +27,40 @@ describe('<FormFieldOptionGroup>', () => {
 
     it('renders component', () => {
       cy.dataCy('form-field-option-group').should('be.visible');
+    });
+
+    it('renders 3 options', () => {
+      cy.dataCy('form-field-option').should('have.length', 3);
+    });
+
+    it('renders options with coorect styles', () => {
+      cy.dataCy('form-field-option')
+        .first()
+        .should('have.backgroundColor', white)
+        .should('have.css', 'border-radius', borderRadius);
+      cy.dataCy('form-field-option-icon').invoke('height').should('equal', 48);
+      cy.dataCy('form-field-option-icon').invoke('width').should('equal', 48);
+    });
+
+    it('shows highlighted option', () => {
+      cy.dataCy('form-field-option').first().click();
+      cy.dataCy('form-field-option')
+        .first()
+        .should('have.css', 'border-color', hexToRgb(colorPrimary));
+      cy.dataCy('form-field-option-check')
+        .first()
+        .find('i')
+        .should('be.visible')
+        .should('have.css', 'color', hexToRgb(colorPrimary));
+      cy.dataCy('form-field-option').last().click();
+      cy.dataCy('form-field-option')
+        .last()
+        .should('have.css', 'border-color', hexToRgb(colorPrimary));
+      cy.dataCy('form-field-option-check')
+        .last()
+        .find('i')
+        .should('be.visible')
+        .should('have.css', 'color', hexToRgb(colorPrimary));
     });
   });
 });
