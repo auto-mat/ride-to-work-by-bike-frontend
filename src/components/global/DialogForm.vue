48fe81c2c4f9bc5e1d5319920df6c88f2d467f26
Šimon Macek <simon.macek@proficio.cz>
2024-01-24 12:30:42 -0500

    components/global: add DialogForm component

---
+++ src/components/global/DialogForm.vue
@@ -0,0 +1,170 @@
+<script lang="ts">
+/**
+ * DialogForm Component
+ *
+ * The `DialogForm` component is used to render a dialog window that can be
+ * customized with various slots. Supports validation.
+ *
+ * It provides slots for the following content:
+ *
+ * - `title`: For the title of the dialog.
+ * - `content`: For the main content of the dialog.
+ * - `buttons`: For action buttons.
+ *
+ * The `content` and `image` slots are rendered side by side within
+ * a scrollable dialog window (on desktop screens).
+ *
+ * @props
+ * - `modelValue` (Boolean, required): Controls the visibility of the dialog.
+ * - `formRef` (Object, required): Template reference to the contained form.
+ *
+ * @events
+ * - `update:modelValue`: Emitted as a part of v-model structure
+ * - `change`: Emitted when the dialog is closed or opened
+ *
+ * @example
+ * <dialog-card>
+ *   <template v-slot:title>
+ *     <!-- Title content here -->
+ *   </template>
+ *   <template v-slot:content>
+ *     <!-- Main content here -->
+ *   </template>
+ *   <template v-slot:buttons>
+ *     <!-- Action buttons here -->
+ *   </template>
+ * </dialog-form>
+ *
+ * @see [Figma Design](https://www.figma.com/file/L8dVREySVXxh3X12TcFDdR/Do-pr%C3%A1ce-na-kole?type=design&node-id=6014%3A46223&mode=dev)
+ */
+
+// libraries
+import { defineComponent, computed } from 'vue';
+import { QForm } from 'quasar';
+
+export default defineComponent({
+  name: 'DialogForm',
+  props: {
+    formRef: {
+      type: Object as () => typeof QForm | null,
+      required: false,
+    },
+    modelValue: {
+      type: Boolean,
+      required: true,
+    },
+  },
+  emits: ['change', 'form-submit', 'update:modelValue'],
+  setup(props, { emit }) {
+    // determines if dialog window is open
+    const isOpen = computed({
+      get: (): boolean => props.modelValue,
+      set: (value: boolean): void => {
+        emit('update:modelValue', value);
+        emit('change');
+      },
+    });
+
+    // closes the dialog window
+    const close = (): void => {
+      isOpen.value = false;
+    };
+
+    /**
+     * Validates the form.
+     * If form is valid emits `form-submit` event and closes dialog.
+     */
+    const submit = async (): Promise<void> => {
+      if (!props.formRef || !props.formRef) {
+        return;
+      }
+      const isFormValid: boolean = await props.formRef.validate();
+      if (isFormValid) {
+        emit('form-submit');
+        close();
+      } else {
+        props.formRef.$el.scrollIntoView({
+          behavior: 'smooth',
+        });
+      }
+    };
+
+    return {
+      isOpen,
+      close,
+      submit,
+    };
+  },
+});
+</script>
+
+<template>
+  <q-dialog square persistent v-model="isOpen" data-cy="dialog-form">
+    <q-card class="relative-position overflow-visible bg-white">
+      <!-- Section: Card header -->
+      <q-card-section
+        v-if="$slots.title"
+        class="q-pt-none"
+        data-cy="dialog-header"
+      >
+        <!-- Title -->
+        <h3 class="text-h6 q-mt-sm q-pt-xs q-mb-none">
+          <slot name="title" />
+        </h3>
+      </q-card-section>
+
+      <q-separator />
+
+      <!-- Section: Card body -->
+      <q-card-section
+        v-if="$slots.content || $slots.buttons"
+        class="scroll"
+        data-cy="dialog-body"
+        style="max-height: 80vh"
+      >
+        <!-- Content -->
+        <slot v-if="$slots.content" name="content"></slot>
+        <!-- Buttons -->
+        <slot
+          v-if="$slots.buttons"
+          name="buttons"
+          :close="close"
+          :submit="submit"
+        ></slot>
+      </q-card-section>
+
+      <!-- Button: Close dialog -->
+      <q-card-actions
+        class="dialog-close__wrapper inline-block absolute-top-right q-px-none q-py-none"
+      >
+        <q-btn
+          v-close-popup
+          round
+          unelevated
+          color="blue-grey-1"
+          icon="close"
+          class="dialog-close text-grey-10"
+          data-cy="dialog-close"
+        />
+      </q-card-actions>
+    </q-card>
+  </q-dialog>
+</template>
+
+<style lang="scss" scoped>
+.q-dialog__inner > div {
+  overflow: visible !important;
+}
+
+.dialog-close__wrapper {
+  top: -19px;
+  right: -19px;
+}
+
+.dialog-close {
+  width: 38px;
+  height: 38px;
+  min-width: 38px;
+  min-height: 38px;
+}
+</style>
