5b3a7d44b1e24339523863b8d0ba9424a0a8c0e9
Šimon Macek <simon.macek@proficio.cz>
2024-02-23 00:16:38 -0500

    components/global/DialogDefault: doc @props

--- src/components/global/DialogDefault.vue
+++ src/components/global/DialogDefault.vue
@@ -11,7 +11,11 @@
  * - `content`: For the main content of the dialog.
  *
  * @props
+ * - `horizontal` (Boolean, default: false): Determines if the dialog should
+ *   display content horizontally.
  * - `modelValue` (Boolean, required): Controls the visibility of the dialog.
+ * - `noPadding` (Boolean, default: false): Determines if the dialog content
+ *   should have padding.
  *
  * @events
  * - `update:modelValue`: Emitted as a part of v-model structure
