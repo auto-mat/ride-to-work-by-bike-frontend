7c0b92051c2db826ca1e201451e34a5ba840c642
Šimon Macek <simon.macek@proficio.cz>
2024-04-10 18:11:15 -0400

    routes page text

--- src/pages/RoutesPage.vue
+++ src/pages/RoutesPage.vue
@@ -1,6 +1,28 @@
 <template>
-  <main></main>
+  <q-page class="overflow-hidden bg-white" data-cy="q-main">
+    <div class="q-px-lg">
+      <h1
+        class="text-h5 q-mt-none q-pt-lg text-weight-bold"
+        data-cy="routes-page-title"
+      >
+        {{ $t('routes.titleRoutes') }}
+      </h1>
+      <p
+        v-html="$t('routes.descriptionRoutes')"
+        data-cy="routes-page-description"
+      ></p>
+    </div>
+    <div></div>
+  </q-page>
 </template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'RoutesPage',
+});
+</script>
 
 <style lang="scss" scoped>
 main {
