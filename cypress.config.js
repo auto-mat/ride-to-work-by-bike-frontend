5955d18fccc9cf6d00f6f6a00b4b68b3efb7ed44
Šimon Macek <simon.macek@proficio.cz>
2024-04-18 17:00:52 -0400

    cleanup

--- cypress.config.js
+++ cypress.config.js
@@ -30,7 +30,6 @@ module.exports = defineConfig({
     supportFile: 'test/cypress/support/e2e.js',
     specPattern: 'test/cypress/e2e/**/*.cy.{js,jsx,ts,tsx}',
     defaultCommandTimeout: 60000,
-    watchForFileChanges: true,
   },
   component: {
     setupNodeEvents(on, config) {
