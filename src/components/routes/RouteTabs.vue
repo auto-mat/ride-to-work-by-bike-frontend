ae23e1df69c23164a0684186c0ace4cfb0a0eb1f
Šimon Macek <simon.macek@proficio.cz>
2024-05-10 16:09:19 +0200

    route-tabs add new content

--- src/components/routes/RouteTabs.vue
+++ src/components/routes/RouteTabs.vue
@@ -11,6 +11,7 @@
  * - `app`: For a view of app links.
  *
  * @components
+ * - `RoutesApps`: Component to render the Apps view.
  * - `RoutesCalendar`: Component to render the calendar view.
  * - `RouteListDisplay`: Component to render the list view (display mode)
  * - `RoutesListEdit`: Component to render the list view (edit mode)
@@ -25,7 +26,9 @@
 import { defineComponent, ref } from 'vue';
 
 // components
+import RoutesApps from './RoutesApps.vue';
 import RouteListDisplay from './RouteListDisplay.vue';
+import RouteListEdit from './RouteListEdit.vue';
 
 // types
 import type { RouteItem, RouteTab } from '../types/Route';
@@ -41,7 +44,9 @@ const routeList: RouteItem[] = routeListFixture as RouteItem[];
 export default defineComponent({
   name: 'RouteTabs',
   components: {
+    RoutesApps,
     RouteListDisplay,
+    RouteListEdit,
   },
   props: {
     locked: {
@@ -134,6 +139,7 @@ export default defineComponent({
       <!-- Panel: List -->
       <q-tab-panel name="list" data-cy="route-tabs-panel-list">
         <div class="text-h6">{{ $t('routes.tabList') }}</div>
+        <route-list-edit :routes="routeList" />
         <route-list-display :routes="routeList" />
       </q-tab-panel>
       <!-- Panel: Map -->
@@ -143,6 +149,7 @@ export default defineComponent({
       <!-- Panel: App -->
       <q-tab-panel name="app" data-cy="route-tabs-panel-app">
         <div class="text-h6">{{ $t('routes.tabApp') }}</div>
+        <routes-apps />
       </q-tab-panel>
     </q-tab-panels>
   </div>
