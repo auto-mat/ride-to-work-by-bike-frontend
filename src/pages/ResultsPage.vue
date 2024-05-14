5fd8b8b68f1e2b612c43700c16cbda92bdac411b
Šimon Macek <simon.macek@proficio.cz>
2024-05-14 18:56:24 +0200

    extract upcoming challenge state component

--- src/pages/ResultsPage.vue
+++ src/pages/ResultsPage.vue
@@ -4,31 +4,26 @@
  *
  * Shows upcoming challenges, badges, user progress, and other results.
  *
+ * @components
+ * - `ResultsChallengeUpcoming`: Component to render content in
+ * the "challenge-upcoming" state.
+ *
  * @see [Figma Design](https://www.figma.com/file/L8dVREySVXxh3X12TcFDdR/Do-pr%C3%A1ce-na-kole?type=design&node-id=4948%3A125553&mode=dev)
  **/
 
 // libraries
 import { defineComponent } from 'vue';
 
-// components
-import BadgeAchievement from 'src/components/homepage/BadgeAchievement.vue';
-import CardChallenge from 'src/components/homepage/CardChallenge.vue';
-import SectionColumns from 'src/components/homepage/SectionColumns.vue';
-
-// mocks
-import { badgeList, cardsChallenge } from '../mocks/homepage';
+import ResultsChallengeUpcoming from 'components/results/ResultsChallengeUpcoming.vue';
 
 export default defineComponent({
   name: 'ResultsPage',
   components: {
-    BadgeAchievement,
-    CardChallenge,
-    SectionColumns,
+    ResultsChallengeUpcoming,
   },
   setup() {
     return {
-      badgeList,
-      cardsChallenge,
+      state: 'challenge-upcoming',
     };
   },
 });
@@ -44,75 +39,11 @@ export default defineComponent({
       >
         {{ $t('results.titleResults') }}
       </h1>
-      <!-- Section: Upcoming challenges -->
-      <div>
-        <!-- Title -->
-        <h2
-          class="text-h6 q-mt-none text-weight-bold q-pt-xl"
-          data-cy="upcoming-challenges-title"
-        >
-          {{ $t('results.titleUpcomingChallenges') }}
-        </h2>
-        <!-- Cards: Challenge -->
-        <section-columns
-          :columns="3"
-          class="q-col-gutter-lg q-pb-xl"
-          data-cy="upcoming-challenges"
-        >
-          <card-challenge
-            v-for="card in cardsChallenge"
-            :key="card.title"
-            :card="card"
-            data-cy="card-challenge"
-          />
-        </section-columns>
-      </div>
-      <!-- Section: Badges -->
-      <div>
-        <!-- Title -->
-        <h2
-          class="text-h6 q-mt-none text-weight-bold q-pt-xl"
-          data-cy="badges-title"
-        >
-          {{ $t('results.titleBadges') }}
-        </h2>
-        <!-- Badges: Achievement -->
-        <section-columns
-          :columns="4"
-          class="q-col-gutter-lg q-pt-xl q-pb-xl"
-          data-cy="list-badges"
-        >
-          <badge-achievement
-            v-for="badge in badgeList"
-            :key="badge.title"
-            :badge="badge"
-            class="full-width"
-            data-cy="badge-item"
-          />
-        </section-columns>
-      </div>
-      <!-- Section: Past challenges -->
-      <div>
-        <!-- Title -->
-        <h2
-          class="text-h6 q-mt-none text-weight-bold q-pt-xl"
-          data-cy="past-challenges-title"
-        >
-          {{ $t('results.titlePastChallenges') }}
-        </h2>
-        <!-- Button -->
-        <q-btn
-          rounded
-          unelevated
-          outline
-          color="primary"
-          class="q-mt-lg"
-          data-cy="past-challenges-button"
-        >
-          {{ $t('results.buttonPastChallenges') }}
-          <q-icon name="arrow_forward" size="18px" class="q-ml-sm" />
-        </q-btn>
-      </div>
+
+      <results-challenge-upcoming
+        v-if="state === 'challenge-upcoming'"
+        data-cy="results-challenge-upcoming"
+      />
     </div>
   </q-page>
 </template>
--- test/cypress/e2e/results.spec.cy.js
+++ test/cypress/e2e/results.spec.cy.js
@@ -45,37 +45,7 @@ function coreTests() {
   });
 
   it('renders upcoming challenges section', () => {
-    let i18n;
-    cy.window().should('have.property', 'i18n');
-    cy.window()
-      .then((win) => {
-        i18n = win.i18n;
-      })
-      .then(() => {
-        cy.dataCy('upcoming-challenges-title').should('be.visible');
-        cy.dataCy('upcoming-challenges').should('be.visible');
-
-        // State: Before challenge
-        // title challenges
-        cy.dataCy('upcoming-challenges-title')
-          .should('be.visible')
-          .and('contain', i18n.global.t('results.titleUpcomingChallenges'));
-        // list challenges
-        cy.dataCy('upcoming-challenges').should('be.visible');
-        // title badges
-        cy.dataCy('badges-title')
-          .should('be.visible')
-          .and('contain', i18n.global.t('results.titleBadges'));
-        // list badges
-        cy.dataCy('list-badges').should('be.visible');
-        // title past challenges
-        cy.dataCy('past-challenges-title')
-          .should('be.visible')
-          .and('contain', i18n.global.t('results.titlePastChallenges'));
-        // button past challenges
-        cy.dataCy('past-challenges-button')
-          .should('be.visible')
-          .and('contain', i18n.global.t('results.buttonPastChallenges'));
-      });
+    // upcoming challenges
+    cy.dataCy('results-challenge-upcoming').should('be.visible');
   });
 }
