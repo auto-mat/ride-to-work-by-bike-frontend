56548ff1f9f9d0fc9e5bb8029e4aabfb261b4b58
Šimon Macek <simon.macek@proficio.cz>
2024-05-15 18:10:16 +0200

    core component

---
+++ src/components/__tests__/ResultsList.cy.js
@@ -0,0 +1,36 @@
+import ResultsList from 'components/results/ResultsList.vue';
+import { i18n } from '../../boot/i18n';
+
+describe('<ResultsList>', () => {
+  it('has translation for all strings', () => {
+    cy.testLanguageStringsInContext([], 'index.component', i18n);
+  });
+
+  context('desktop', () => {
+    beforeEach(() => {
+      cy.mount(ResultsList, {
+        props: {},
+      });
+      cy.viewport('macbook-16');
+    });
+
+    coreTests();
+  });
+
+  context('mobile', () => {
+    beforeEach(() => {
+      cy.mount(ResultsList, {
+        props: {},
+      });
+      cy.viewport('iphone-6');
+    });
+
+    coreTests();
+  });
+});
+
+function coreTests() {
+  it('renders component', () => {
+    cy.dataCy('results-list').should('be.visible');
+  });
+}
