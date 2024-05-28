a89802b6d2ffb20ec57e3340f67917295f18ec3f
Šimon Macek <simon.macek@proficio.cz>
2024-05-28 14:56:21 +0200

    community page boilerplate

---
+++ src/pages/CommunityPage.vue
@@ -0,0 +1,65 @@
+<script lang="ts">
+/**
+ * CommunityPage Component
+ *
+ * The `CommunityPage` displays information about community events.
+ *
+ * @components
+ * - `LoginRegisterHeader`: Header for login and registration pages.
+ *
+ * @layout
+ * - `LoginRegisterLayout`: Layout for login and registration pages.
+ *
+ * @see [Figma Design](https://www.figma.com/design/L8dVREySVXxh3X12TcFDdR/Do-pr%C3%A1ce-na-kole?node-id=4858-104327&t=ZZSrUuLgRLYixhUu-1)
+ */
+import { defineComponent, ref } from 'vue';
+
+export default defineComponent({
+  name: 'CommunityPage',
+  setup() {
+    const city = ref<string | null>(null);
+
+    return {
+      city,
+    };
+  },
+});
+</script>
+
+<template>
+  <q-page class="overflow-hidden" data-cy="q-main">
+    <div class="q-px-lg bg-white">
+      <!-- Section title -->
+      <div class="col-12 flex flex-wrap items-center justify-between gap-4">
+        <!-- Page title -->
+        <h1
+          class="text-h5 text-bold text-white q-my-none"
+          data-cy="community-page-title"
+        >
+          {{ $t('community.titleCommunity') }}
+        </h1>
+
+        <!-- Select: City -->
+        <FormFieldSelectCity v-model="city" data-cy="community-select-city" />
+      </div>
+
+      <!-- Section: Local events -->
+      <div>
+        <h2 class="text-body1 text-bold q-my-none" data-cy="info-title">
+          {{ $t('community.titleLocalEvents') }}
+        </h2>
+        <ListEventsLocal />
+      </div>
+
+      <!-- TODO: Section Forum -->
+
+      <!-- TODO: Section Locations -->
+
+      <!-- TODO: Section Social networks -->
+
+      <!-- TODO: Section News -->
+
+      <!-- TODO: Section Instagram -->
+    </div>
+  </q-page>
+</template>
---
+++ test/cypress/e2e/community.spec.cy.js
@@ -0,0 +1,63 @@
+import { routesConf } from '../../../src/router/routes_conf';
+
+describe('Community page', () => {
+  context('desktop', () => {
+    beforeEach(() => {
+      cy.visit('#' + routesConf['community']['path']);
+      cy.viewport('macbook-16');
+
+      // load config an i18n objects as aliases
+      cy.task('getAppConfig', process).then((config) => {
+        // alias config
+        cy.wrap(config).as('config');
+        cy.window().should('have.property', 'i18n');
+        cy.window().then((win) => {
+          // alias i18n
+          cy.wrap(win.i18n).as('i18n');
+        });
+      });
+    });
+
+    coreTests();
+
+    it('renders left drawer', () => {
+      cy.dataCy('q-drawer').should('be.visible');
+      cy.dataCy('drawer-header').should('be.visible');
+      cy.dataCy('user-select').should('be.visible');
+      cy.dataCy('drawer-toggle-buttons').should('be.visible');
+      cy.dataCy('drawer-menu').should('be.visible');
+    });
+  });
+
+  context('mobile', () => {
+    beforeEach(() => {
+      cy.visit('#' + routesConf['community']['path']);
+      cy.viewport('iphone-6');
+
+      // load config an i18n objects as aliases
+      cy.task('getAppConfig', process).then((config) => {
+        // alias config
+        cy.wrap(config).as('config');
+        cy.window().should('have.property', 'i18n');
+        cy.window().then((win) => {
+          // alias i18n
+          cy.wrap(win.i18n).as('i18n');
+        });
+      });
+    });
+
+    coreTests();
+  });
+});
+
+function coreTests() {
+  it('renders page heading section', () => {
+    cy.get('@i18n').then((i18n) => {
+      // title
+      cy.dataCy('community-page-title')
+        .should('be.visible')
+        .and('contain', i18n.global.t('community.titleCommunity'));
+      // cy.dataCy('community-select-city').should('be.visible');
+    });
+  });
+}
