4a68c4639dfeef4436809870006fff59d626a682
Šimon Macek <simon.macek@proficio.cz>
2024-06-05 16:37:56 +0300

    select city input component

---
+++ src/components/__tests__/FormFieldSelectCity.cy.js
@@ -0,0 +1,43 @@
+import FormFieldTestWrapper from 'components/global/FormFieldTestWrapper.vue';
+import { i18n } from '../../boot/i18n';
+
+describe('<FormFieldSelectCity>', () => {
+  it('has translation for all strings', () => {
+    cy.testLanguageStringsInContext(['labelCity'], 'form', i18n);
+  });
+
+  context('desktop', () => {
+    beforeEach(() => {
+      cy.mount(FormFieldTestWrapper, {
+        props: {
+          component: 'FormFieldSelectCity',
+        },
+      });
+      cy.viewport('macbook-16');
+    });
+
+    coreTests();
+  });
+
+  context('mobile', () => {
+    beforeEach(() => {
+      cy.mount(FormFieldTestWrapper, {
+        props: {
+          component: 'FormFieldSelectCity',
+        },
+      });
+      cy.viewport('iphone-6');
+    });
+
+    coreTests();
+  });
+});
+
+function coreTests() {
+  it('renders component', () => {
+    cy.dataCy('form-field-select-city').should('be.visible');
+    cy.dataCy('form-select-label')
+      .should('be.visible')
+      .and('contain', i18n.global.t('form.labelCity'));
+  });
+}
