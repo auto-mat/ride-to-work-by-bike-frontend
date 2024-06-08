7014a017ec446263fe04b5c14a0769399d2f1ff8
Šimon Macek <simon.macek@proficio.cz>
2024-06-08 09:38:45 +0300

    company coordinator page boilerplate

---
+++ src/pages/CompanyCoordinatorPage.vue
@@ -0,0 +1,57 @@
+<script lang="ts">
+/**
+ * CompanyCoordinatorPage Component
+ *
+ * The `CompanyCoordinatorPage` displays information about community events.
+ *
+ * @components
+ *
+ * @layout
+ * - `MainLayout`: Default layout with sidebar on desktop.
+ *
+ * @see [Figma Design](https://www.figma.com/design/L8dVREySVXxh3X12TcFDdR/Do-pr%C3%A1ce-na-kole?node-id=4858-104327&t=ZZSrUuLgRLYixhUu-1)
+ */
+import { defineComponent } from 'vue';
+
+// components
+
+// // composables
+// import { i18n } from '../boot/i18n';
+
+// // config
+// import { routesConf } from '../router/routes_conf';
+
+// // types
+// import type { FormOption } from '../components/types/Form';
+
+export default defineComponent({
+  name: 'CompanyCoordinatorPage',
+  components: {},
+  setup() {
+    return {};
+  },
+});
+</script>
+
+<template>
+  <q-page class="overflow-hidden" data-cy="q-main">
+    <div class="q-px-lg bg-white q-pb-xl q-pt-lg">
+      <!-- Section title -->
+      <div class="col-12 flex flex-wrap items-center justify-between gap-16">
+        <!-- Page title -->
+        <div>
+          <h1
+            class="text-h5 q-my-none text-weight-bold"
+            data-cy="community-page-title"
+          >
+            {{ $t('companyCoordinator.titleBecomeCoordinator') }}
+          </h1>
+        </div>
+      </div>
+
+      <!-- TODO: Section description -->
+
+      <!-- TODO: Form apply -->
+    </div>
+  </q-page>
+</template>
---
+++ test/cypress/e2e/coordinator_page.spec.cy.js
@@ -0,0 +1,53 @@
+import { routesConf } from '../../../src/router/routes_conf';
+
+describe('Company coordinator page', () => {
+  context('desktop', () => {
+    beforeEach(() => {
+      cy.visit('#' + routesConf['company_coordinator']['path']);
+      cy.viewport('macbook-16');
+
+      // load config an i18n objects as aliases
+      cy.task('getAppConfig', process).then((config) => {
+        // alias config
+        cy.wrap(config).as('config');
+        cy.window().should('have.property', 'i18n');
+        cy.window().then((win) => {
+          // alias i18n
+          cy.wrap(win.i18n).as('i18n');
+        });
+      });
+    });
+
+    coreTests();
+
+    it('renders left drawer', () => {
+      cy.dataCy('q-drawer').should('be.visible');
+      cy.dataCy('drawer-header').should('be.visible');
+      cy.dataCy('user-select').should('be.visible');
+      cy.dataCy('drawer-toggle-buttons').should('be.visible');
+      cy.dataCy('drawer-menu').should('be.visible');
+    });
+  });
+
+  context('mobile', () => {
+    beforeEach(() => {
+      cy.visit('#' + routesConf['community']['path']);
+      cy.viewport('iphone-6');
+
+      // load config an i18n objects as aliases
+      cy.task('getAppConfig', process).then((config) => {
+        // alias config
+        cy.wrap(config).as('config');
+        cy.window().should('have.property', 'i18n');
+        cy.window().then((win) => {
+          // alias i18n
+          cy.wrap(win.i18n).as('i18n');
+        });
+      });
+    });
+
+    coreTests();
+  });
+});
+
+function coreTests() {}
