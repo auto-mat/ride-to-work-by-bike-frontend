277e275b5e3e167a1ca70abbc9d77e66090bac87
Šimon Macek <simon.macek@proficio.cz>
2024-06-20 08:34:06 +0200

    test cleanup

--- src/components/__tests__/FormFieldCheckboxTeam.cy.js
+++ src/components/__tests__/FormFieldCheckboxTeam.cy.js
@@ -1,11 +1,6 @@
 import FormFieldTestWrapper from '../global/FormFieldTestWrapper.vue';
-import { i18n } from '../../boot/i18n';
 
 describe('<FormFieldCheckboxTeam>', () => {
-  it('has translation for all strings', () => {
-    cy.testLanguageStringsInContext([], 'index.component', i18n);
-  });
-
   context('desktop', () => {
     beforeEach(() => {
       cy.mount(FormFieldTestWrapper, {
