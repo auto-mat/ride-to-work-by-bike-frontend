3a18cc7edec17a9799fc447f9765dfb161b8c2fd
Šimon Macek <simon.macek@proficio.cz>
2024-06-21 10:41:55 +0200

    task list coordinator boilerplate

---
+++ src/components/coordinator/TaskListCoordinator.vue
@@ -0,0 +1,31 @@
+<script lang="ts">
+/**
+ * TaskListCoordinator Component
+ *
+ * @description * Use this component to render a list of tasks
+ * for a company coordinator in the form of a vertical timeline.
+ *
+ * @props
+ * - `tasks` (CoordinatorTask, required): The object representing the tasks.
+ *   It should be of type `CoordinatorTask`.
+ *
+ * @components
+ * - `CHILD`: Component to ... .
+ *
+ * @example
+ * <TaskListCoordinator></TaskListCoordinator>
+ *
+ * @see [Figma Design](...)
+ */
+
+// libraries
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'TaskListCoordinator',
+});
+</script>
+
+<template>
+  <div></div>
+</template>
