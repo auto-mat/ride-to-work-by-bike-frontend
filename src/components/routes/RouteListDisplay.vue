52ced2fa57fa026f3b3cd0ab357e85133b278658
Šimon Macek <simon.macek@proficio.cz>
2024-08-21 09:25:44 +0200

    cleanup

--- src/components/routes/RouteListDisplay.vue
+++ src/components/routes/RouteListDisplay.vue
@@ -65,10 +65,6 @@ export default defineComponent({
     );
 
     return {
-      challengeLoggingWindowDays,
-      challengeStartDate,
-      startDate,
-      endDate,
       days,
       formatDate,
       formatDateName,
