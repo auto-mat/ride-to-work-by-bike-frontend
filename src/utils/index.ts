2f866034ec1cd9d52f50197c548f5fc6d636450a
Šimon Macek <simon.macek@proficio.cz>
2024-10-10 19:45:17 +0200

    fix config import

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -2,7 +2,7 @@
 import type { AxiosRequestHeaders } from 'axios';
 
 // config
-const { rideToWorkByBikeConfig } = await import('../boot/global_vars');
+import rideToWorkByBikeConfig from '../boot/global_vars';
 
 /*
  * Convert date time timestamp number to formated
