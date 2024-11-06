efe52b81226ff3c29a5866514364a6e21ce38ede
Tomas Zigo <tomas.zigo@slovanet.sk>
2024-11-06 14:54:56 +0100

    src/i18n: read i18n date time formats from global app config file
    dateTimeFormatsAllLocales var.

--- src/boot/i18n.js
+++ src/boot/i18n.js
@@ -2,17 +2,18 @@ import { boot } from 'quasar/wrappers';
 
 import { createI18n } from 'vue-i18n';
 
-import { datetimeFormats } from '../utils/datetime_formats';
-
-import { loadLocaleMessages } from '../i18n';
+import { getDateTimeFormats, loadLocaleMessages } from '../i18n';
 import { defaultLocale, fallbackLocale } from 'src/i18n/def_locale';
+
+const messages = await loadLocaleMessages();
+const datetimeFormats = getDateTimeFormats(Object.keys(messages));
 
 // Create I18n instance
 export const i18n = createI18n({
   locale: defaultLocale,
   fallbackLocale: fallbackLocale,
   globalInjection: true,
-  messages: loadLocaleMessages(),
+  messages: messages,
   datetimeFormats,
 });
 
