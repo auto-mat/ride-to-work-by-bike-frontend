333d3b18218e646bfc97ed50cdadb24fb41bf8fb
Šimon Macek <simon.macek@proficio.cz>
2024-11-13 12:57:53 +0100

    cleanup

--- src/components/global/FormFieldCompany.vue
+++ src/components/global/FormFieldCompany.vue
@@ -394,7 +394,7 @@ export default defineComponent({
       class="text-caption text-bold"
       data-cy="form-field-company-label"
     >
-      {{ formFieldLabel }}{{ company }}
+      {{ formFieldLabel }}
     </label>
     <div class="row">
       <div class="col-12 col-sm" data-cy="col-input">
