40139ffc11ecad489c5e00c6bb6eff53317ece5c
Šimon Macek <simon.macek@proficio.cz>
2024-11-25 21:33:24 +0100

    add get cities composable

---
+++ src/composables/useApiGetCities.ts
@@ -0,0 +1,110 @@
+// libraries
+import { ref } from 'vue';
+
+// composables
+import { useApi } from './useApi';
+
+// config
+import { rideToWorkByBikeConfig } from '../boot/global_vars';
+
+// stores
+import { useLoginStore } from '../stores/login';
+
+// types
+import type { FormSelectOption } from '../components/types/Form';
+import type { GetCitiesResponse } from '../components/types/City';
+import type { Logger } from '../components/types/Logger';
+
+// utils
+import { requestDefaultHeader, requestTokenHeader } from '../utils';
+
+export const useApiGetCities = (logger: Logger) => {
+  const options = ref<FormSelectOption[]>([]);
+  const isLoading = ref<boolean>(false);
+  const loginStore = useLoginStore();
+  const { apiFetch } = useApi();
+
+  /**
+   * Load cities options
+   * Fetches cities and saves them into options
+   */
+  const loadCities = async (): Promise<void> => {
+    // reset options
+    options.value = [];
+
+    // get cities
+    isLoading.value = true;
+
+    // append access token into HTTP header
+    const requestTokenHeader_ = { ...requestTokenHeader };
+    requestTokenHeader_.Authorization += loginStore.getAccessToken;
+
+    // fetch cities
+    const { data } = await apiFetch<GetCitiesResponse>({
+      endpoint: `${rideToWorkByBikeConfig.urlApiCities}`,
+      method: 'get',
+      translationKey: 'getCities',
+      showSuccessMessage: false,
+      headers: Object.assign(requestDefaultHeader(), requestTokenHeader_),
+      logger,
+    });
+
+    if (data?.results?.length) {
+      pushResultsToOptions(data);
+    }
+
+    // if data has multiple pages, fetch all pages
+    if (data?.next) {
+      await fetchNextPage(data.next);
+    }
+
+    isLoading.value = false;
+  };
+
+  /**
+   * Fetch next page of cities
+   */
+  const fetchNextPage = async (url: string): Promise<void> => {
+    // append access token into HTTP header
+    const requestTokenHeader_ = { ...requestTokenHeader };
+    requestTokenHeader_.Authorization += loginStore.getAccessToken;
+
+    // fetch next page
+    const { data } = await apiFetch<GetCitiesResponse>({
+      endpoint: url,
+      method: 'get',
+      translationKey: 'getCities',
+      showSuccessMessage: false,
+      headers: Object.assign(requestDefaultHeader(), requestTokenHeader_),
+      logger,
+    });
+
+    // store results
+    if (data?.results?.length) {
+      pushResultsToOptions(data);
+    }
+
+    // if data has multiple pages, fetch all pages
+    if (data?.next) {
+      await fetchNextPage(data.next);
+    }
+  };
+
+  /**
+   * Push results to options
+   */
+  const pushResultsToOptions = (data: GetCitiesResponse): void => {
+    const pageResults = data.results.map((city) => ({
+      label: city.name,
+      value: city.id,
+    }));
+
+    options.value.push(...pageResults);
+  };
+
+  return {
+    options,
+    isLoading,
+    loadCities,
+  };
+};
