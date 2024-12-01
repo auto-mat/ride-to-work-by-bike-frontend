b2a27633e07b14252a138bba83602e3802dd74e6
Tomas Zigo <tomas.zigo@slovanet.sk>
2024-12-01 13:07:40 +0100

    src/composables: fix useApiGetCities component missing TS types, debug messages, func doc header

--- src/composables/useApiGetCities.ts
+++ src/composables/useApiGetCities.ts
@@ -1,5 +1,5 @@
 // libraries
-import { ref } from 'vue';
+import { ref, Ref } from 'vue';
 
 // composables
 import { useApi } from './useApi';
@@ -17,7 +17,21 @@ import type { Logger } from '../components/types/Logger';
 // utils
 import { requestDefaultHeader, requestTokenHeader } from '../utils';
 
-export const useApiGetCities = (logger: Logger | null) => {
+type useApiGetCitiesReturn = {
+  cities: Ref<City[]>;
+  isLoading: Ref<boolean>;
+  loadCities: () => Promise<void>;
+};
+
+/**
+ * Get cities composable
+ * Used to getting API cities data
+ * @param logger - Logger
+ * @returns {useApiGetCitiesReturn}
+ */
+export const useApiGetCities = (
+  logger: Logger | null,
+): useApiGetCitiesReturn => {
   const cities = ref<City[]>([]);
   const isLoading = ref<boolean>(false);
   const loginStore = useLoginStore();
@@ -29,9 +43,17 @@ export const useApiGetCities = (logger: Logger | null) => {
    */
   const loadCities = async (): Promise<void> => {
     // reset options
+    logger?.debug(
+      `Reseting default options <${JSON.stringify(cities.value, null, 2)}>.`,
+    );
+    // reset options
     cities.value = [];
+    logger?.debug(
+      `Default options set to <${JSON.stringify(cities.value, null, 2)}>.`,
+    );
 
     // get cities
+    logger?.info('Get cities from the API.');
     isLoading.value = true;
 
     // append access token into HTTP header
@@ -62,8 +84,11 @@ export const useApiGetCities = (logger: Logger | null) => {
 
   /**
    * Fetch next page of cities
+   * @param {string} url - Get cities next page API URL
+   * @returns {Promise<void>} - Promise
    */
   const fetchNextPage = async (url: string): Promise<void> => {
+    logger?.debug(`Fetching next page of cities from <${url}>.`);
     // append access token into HTTP header
     const requestTokenHeader_ = { ...requestTokenHeader };
     requestTokenHeader_.Authorization += loginStore.getAccessToken;
