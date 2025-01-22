b10595df5cc5481ea16d676abeaa7d31f08ee7ff
Šimon Macek <simon.macek@proficio.cz>
2025-01-22 21:25:23 +0100

    password reset page

---
+++ src/components/__tests__/ResetPassword.cy.js
@@ -0,0 +1,36 @@
+import ResetPassword from 'components/register/ResetPassword.vue';
+import { i18n } from '../../boot/i18n';
+
+describe('<ResetPassword>', () => {
+  it('has translation for all strings', () => {
+    cy.testLanguageStringsInContext([], 'index.component', i18n);
+  });
+
+  context('desktop', () => {
+    beforeEach(() => {
+      cy.mount(ResetPassword, {
+        props: {},
+      });
+      cy.viewport('macbook-16');
+    });
+
+    coreTests();
+  });
+
+  context('mobile', () => {
+    beforeEach(() => {
+      cy.mount(ResetPassword, {
+        props: {},
+      });
+      cy.viewport('iphone-6');
+    });
+
+    coreTests();
+  });
+});
+
+function coreTests() {
+  it('renders component', () => {
+    cy.dataCy('component').should('be.visible');
+  });
+}
---
+++ src/components/register/ResetPassword.vue
@@ -0,0 +1,22 @@
+<script lang="ts">
+/**
+ * ResetPassword Component
+ *
+ * @description * Use this component to ... .
+ * You can adjust its appearance by ... .
+ *
+ * @example
+ * <reset-password />
+ */
+
+// libraries
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'ResetPassword',
+});
+</script>
+
+<template>
+  <div></div>
+</template>
