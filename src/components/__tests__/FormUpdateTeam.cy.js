ad53023e27002693f525e8d4acf64e46a641045c
Šimon Macek <simon.macek@proficio.cz>
2025-03-10 22:02:45 +0100

    add new form component and enable update team functionality

---
+++ src/components/__tests__/FormUpdateTeam.cy.js
@@ -0,0 +1,36 @@
+import FormUpdateTeam from 'components/form/FormUpdateTeam.vue';
+import { i18n } from '../../boot/i18n';
+
+describe('<FormUpdateTeam>', () => {
+  it('has translation for all strings', () => {
+    cy.testLanguageStringsInContext([], 'index.component', i18n);
+  });
+
+  context('desktop', () => {
+    beforeEach(() => {
+      cy.mount(FormUpdateTeam, {
+        props: {},
+      });
+      cy.viewport('macbook-16');
+    });
+
+    coreTests();
+  });
+
+  context('mobile', () => {
+    beforeEach(() => {
+      cy.mount(FormUpdateTeam, {
+        props: {},
+      });
+      cy.viewport('iphone-6');
+    });
+
+    coreTests();
+  });
+});
+
+function coreTests() {
+  it('renders component', () => {
+    cy.dataCy('component').should('be.visible');
+  });
+}
