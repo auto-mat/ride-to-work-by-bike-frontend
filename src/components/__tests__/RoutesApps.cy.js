540990691520a20541c860d85c1ee51dd4912160
Šimon Macek <simon.macek@proficio.cz>
2025-04-14 15:25:22 +0200

    add app link test

--- src/components/__tests__/RoutesApps.cy.js
+++ src/components/__tests__/RoutesApps.cy.js
@@ -90,70 +90,81 @@ describe('<RoutesApps>', () => {
 function coreTests() {
   it('renders component', () => {
     cy.fixture('apiGetOpenAppWithRestTokenCyclers').then((responseCyclers) => {
-      cy.waitForOpenAppWithRestTokenApi(responseCyclers);
+      cy.fixture('apiGetOpenAppWithRestTokenNaKolePrahou').then(
+        (responseNaKolePrahou) => {
+          // wait for API calls
+          cy.waitForOpenAppWithRestTokenApi(responseCyclers);
+          cy.waitForOpenAppWithRestTokenApi(responseNaKolePrahou);
+          // component
+          cy.dataCy('routes-apps').should('be.visible');
+          // title automatic
+          cy.dataCy('routes-apps-title-auto')
+            .find('[data-cy="section-heading-title"]')
+            .should('be.visible')
+            .and('contain.text', i18n.global.t('routes.titleAutomaticLogging'));
+          // hint automatic
+          cy.dataCy('routes-apps-title-auto')
+            .find('[data-cy="section-heading-perex"]')
+            .should('be.visible')
+            .and('contain.text', i18n.global.t('routes.hintAutomaticLogging'));
+          cy.dataCy('banner-routes-app')
+            .should('be.visible')
+            .should('have.length', 2);
+          // first button - Cyclers
+          cy.dataCy('banner-routes-app-button')
+            .first()
+            .should('have.attr', 'href', responseCyclers.app_url)
+            .and('have.attr', 'target', '_blank');
+          // second button - Na kole prahou
+          cy.dataCy('banner-routes-app-button')
+            .last()
+            .should('have.attr', 'href', responseNaKolePrahou.app_url)
+            .and('have.attr', 'target', '_blank');
+          // // title manual
+          // cy.dataCy('routes-apps-title-manual')
+          //   .find('[data-cy="section-heading-title"]')
+          //   .should('be.visible')
+          //   .and('contain.text', i18n.global.t('routes.titleManualLogging'));
+          // // hint manual
+          // cy.dataCy('routes-apps-title-manual')
+          //   .find('[data-cy="section-heading-perex"]')
+          //   .should('be.visible')
+          //   .and('contain.text', i18n.global.t('routes.hintManualLogging'));
+          // // buttons container
+          // cy.dataCy('routes-apps-buttons')
+          //   .should('have.css', 'display', 'flex')
+          //   .and('have.css', 'flex-wrap', 'wrap')
+          //   .and('have.css', 'gap', '16px');
+          // // buttton google play
+          // cy.dataCy('routes-apps-google-play')
+          //   .should('be.visible')
+          //   .invoke('height')
+          //   .should('be.equal', 40);
+          // cy.dataCy('routes-apps-google-play')
+          //   .find('img')
+          //   .should('be.visible')
+          //   .then(($img) => {
+          //     cy.testImageHeight($img);
+          //   });
+          // cy.dataCy('routes-apps-google-play')
+          //   .should('have.attr', 'href', urlGooglePlay)
+          //   .and('have.attr', 'target', '_blank');
+          // // buttons app store
+          // cy.dataCy('routes-apps-app-store')
+          //   .should('be.visible')
+          //   .invoke('height')
+          //   .should('be.equal', 40);
+          // cy.dataCy('routes-apps-app-store')
+          //   .find('img')
+          //   .should('be.visible')
+          //   .then(($img) => {
+          //     cy.testImageHeight($img);
+          //   });
+          // cy.dataCy('routes-apps-app-store')
+          //   .should('have.attr', 'href', urlAppStore)
+          //   .and('have.attr', 'target', '_blank');
+        },
+      );
     });
-    cy.fixture('apiGetOpenAppWithRestTokenNaKolePrahou').then(
-      (responseNaKolePrahou) => {
-        cy.waitForOpenAppWithRestTokenApi(responseNaKolePrahou);
-      },
-    );
-    // component
-    cy.dataCy('routes-apps').should('be.visible');
-    // title automatic
-    cy.dataCy('routes-apps-title-auto')
-      .find('[data-cy="section-heading-title"]')
-      .should('be.visible')
-      .and('contain.text', i18n.global.t('routes.titleAutomaticLogging'));
-    // hint automatic
-    cy.dataCy('routes-apps-title-auto')
-      .find('[data-cy="section-heading-perex"]')
-      .should('be.visible')
-      .and('contain.text', i18n.global.t('routes.hintAutomaticLogging'));
-    cy.dataCy('banner-routes-app')
-      .should('be.visible')
-      .should('have.length', 2);
-    // // title manual
-    // cy.dataCy('routes-apps-title-manual')
-    //   .find('[data-cy="section-heading-title"]')
-    //   .should('be.visible')
-    //   .and('contain.text', i18n.global.t('routes.titleManualLogging'));
-    // // hint manual
-    // cy.dataCy('routes-apps-title-manual')
-    //   .find('[data-cy="section-heading-perex"]')
-    //   .should('be.visible')
-    //   .and('contain.text', i18n.global.t('routes.hintManualLogging'));
-    // // buttons container
-    // cy.dataCy('routes-apps-buttons')
-    //   .should('have.css', 'display', 'flex')
-    //   .and('have.css', 'flex-wrap', 'wrap')
-    //   .and('have.css', 'gap', '16px');
-    // // buttton google play
-    // cy.dataCy('routes-apps-google-play')
-    //   .should('be.visible')
-    //   .invoke('height')
-    //   .should('be.equal', 40);
-    // cy.dataCy('routes-apps-google-play')
-    //   .find('img')
-    //   .should('be.visible')
-    //   .then(($img) => {
-    //     cy.testImageHeight($img);
-    //   });
-    // cy.dataCy('routes-apps-google-play')
-    //   .should('have.attr', 'href', urlGooglePlay)
-    //   .and('have.attr', 'target', '_blank');
-    // // buttons app store
-    // cy.dataCy('routes-apps-app-store')
-    //   .should('be.visible')
-    //   .invoke('height')
-    //   .should('be.equal', 40);
-    // cy.dataCy('routes-apps-app-store')
-    //   .find('img')
-    //   .should('be.visible')
-    //   .then(($img) => {
-    //     cy.testImageHeight($img);
-    //   });
-    // cy.dataCy('routes-apps-app-store')
-    //   .should('have.attr', 'href', urlAppStore)
-    //   .and('have.attr', 'target', '_blank');
   });
 }
