1221d4fe43cf1d99103b9e89b0dd19b96df43805
Šimon Macek <simon.macek@proficio.cz>
2025-04-23 23:22:52 +0200

    rework ResultsPage content into ResultsTab component

--- src/pages/ResultsPage.vue
+++ src/pages/ResultsPage.vue
@@ -5,132 +5,39 @@
  * Shows upcoming challenges, badges, user progress, and other results.
  *
  * @components
- * - `ResultsChallengeOngoing`: Component to render content in
- * the "challenge-ongoing" state.
- * - `ResultsChallengeUpcoming`: Component to render content in
- * the "challenge-upcoming" state.
+ * - `PageHeading`: Component to render the page heading.
+ * - `ResultsTabs`: Component to render tabs with results content.
  *
  * @see [Figma Design](https://www.figma.com/file/L8dVREySVXxh3X12TcFDdR/Do-pr%C3%A1ce-na-kole?type=design&node-id=4948%3A125553&mode=dev)
  **/
 
 // libraries
-import { computed, defineComponent, onMounted, ref } from 'vue';
+import { defineComponent } from 'vue';
 
 // components
 import PageHeading from 'components/global/PageHeading.vue';
-
-// config
-import { rideToWorkByBikeConfig } from 'src/boot/global_vars';
-
-// enums
-import {
-  ResultsReportType,
-  ResultsReportTypeByChallenge,
-} from 'src/components/enums/Results';
-
-// stores
-import { useResultsStore } from 'src/stores/results';
+import ResultsTabs from 'components/results/ResultsTabs.vue';
 
 export default defineComponent({
   name: 'ResultsPage',
   components: {
     PageHeading,
+    ResultsTabs,
   },
   setup() {
-    const resultsStore = useResultsStore();
-
-    const { challengeMonth, dataReportIframeHeight } = rideToWorkByBikeConfig;
-
-    onMounted(async () => {
-      // allows Cypress component test to register intercept
-      if (window.Cypress) {
-        await new Promise((resolve) => setTimeout(resolve, 500));
-      }
-
-      resultsStore.loadResultsUrls();
-    });
-
-    const resultsUrls = computed(() => resultsStore.getAvailableReportTypes);
-
-    const activeTab = ref<ResultsReportType | ResultsReportTypeByChallenge>(
-      challengeMonth === 'may'
-        ? ResultsReportTypeByChallenge.may
-        : ResultsReportTypeByChallenge.septemberJanuary,
-    );
-
-    const getResultsUrl = (
-      reportType: ResultsReportType | ResultsReportTypeByChallenge,
-    ) => {
-      return resultsStore.getResultsUrl(reportType)?.data_report_url ?? '';
-    };
-
-    const getReportTypeLabel = (
-      reportType: ResultsReportType | ResultsReportTypeByChallenge,
-    ) => {
-      return resultsStore.getReportTypeLabels[reportType];
-    };
-
-    return {
-      activeTab,
-      dataReportIframeHeight,
-      resultsUrls,
-      resultsStore,
-      getReportTypeLabel,
-      getResultsUrl,
-    };
+    return {};
   },
 });
 </script>
 
 <template>
   <q-page class="overflow-hidden" data-cy="q-main">
-    <div class="q-px-lg q-pt-lg bg-white">
+    <div class="q-px-lg q-pt-lg">
       <!-- Heading -->
       <page-heading data-cy="results-page-title">
         {{ $t('results.titleResults') }}
       </page-heading>
     </div>
-    <!-- Tabs: Report types -->
-    <q-tabs
-      inline-label
-      v-model="activeTab"
-      class="text-grey"
-      active-color="primary"
-      indicator-color="primary"
-      align="center"
-      data-cy="results-tabs"
-    >
-      <q-tab
-        v-for="reportType in resultsUrls"
-        :key="reportType"
-        :name="reportType"
-        :label="getReportTypeLabel(reportType)"
-        :data-cy="`results-tab-${reportType}`"
-      />
-    </q-tabs>
-    <!-- Separator -->
-    <q-separator />
-    <!-- Tab panels: Report types -->
-    <q-tab-panels v-model="activeTab" animated class="q-pb-xl">
-      <q-tab-panel
-        v-for="reportType in resultsUrls"
-        :key="reportType"
-        :name="reportType"
-        :data-cy="`results-tab-panel-${reportType}`"
-      >
-        <div v-if="getResultsUrl(reportType)">
-          <iframe
-            class="full-width"
-            :style="{ height: dataReportIframeHeight }"
-            :src="getResultsUrl(reportType)"
-            frameBorder="0"
-            :data-cy="`results-tab-panel-iframe-${reportType}`"
-          />
-        </div>
-        <div v-else class="text-center text-grey-7 text-body-2 q-mt-lg">
-          {{ $t('results.messageNoReport') }}
-        </div>
-      </q-tab-panel>
-    </q-tab-panels>
+    <results-tabs class="q-pb-xl" />
   </q-page>
 </template>
