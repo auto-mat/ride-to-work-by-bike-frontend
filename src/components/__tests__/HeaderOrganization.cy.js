0d8d9a377e01d829c456311aab44b04785a34c4e
Šimon Macek <simon.macek@proficio.cz>
2025-10-15 11:10:50 +0200

    add mobile tests for header organization

--- src/components/__tests__/HeaderOrganization.cy.js
+++ src/components/__tests__/HeaderOrganization.cy.js
@@ -21,64 +21,73 @@ describe('<HeaderOrganization>', () => {
       cy.viewport('macbook-16');
     });
 
-    testData.forEach((test) => {
-      it(test.description, () => {
-        // initiate store state
-        cy.wrap(useAdminOrganisationStore()).then((adminOrganisationStore) => {
-          const adminOrganisations = computed(
-            () => adminOrganisationStore.getAdminOrganisations,
-          );
-          adminOrganisationStore.setAdminOrganisations(test.storeData);
-          cy.wrap(adminOrganisations)
-            .its('value')
-            .should('deep.equal', test.storeData);
-        });
-        // test elements layout
-        cy.testElementsSideBySide(
-          'header-organization-branch-count',
-          'header-organization-member-count',
-        );
-        // test icon
-        cy.dataCy('header-organization-image-icon').should('be.visible');
-        cy.dataCy('header-organization-image-icon').should(
-          'have.color',
-          secondary,
-        );
-        // test organization data
-        cy.dataCy('header-organization').should('be.visible');
-        cy.dataCy('header-organization-title')
-          .should('be.visible')
-          .and('contain', test.displayData.organizationTitle);
-        // test subsidiaries count
-        cy.dataCy('header-organization-branch-count')
-          .should('be.visible')
-          .and('contain', test.displayData.subsidiariesCount)
-          .and(
-            'contain',
-            i18n.global.t(
-              'coordinator.labelBranches',
-              test.displayData.subsidiariesCount,
-            ),
-          );
-        // test members count
-        cy.dataCy('header-organization-member-count')
-          .should('be.visible')
-          .and('contain', test.displayData.membersCount)
-          .and(
-            'contain',
-            i18n.global.t(
-              'coordinator.labelMembers',
-              test.displayData.membersCount,
-            ),
-          );
-        // test export button
-        cy.dataCy('header-organization-button-export')
-          .should('be.visible')
-          .and('contain', i18n.global.t('coordinator.buttonExportMembers'));
-        cy.dataCy('header-organization-button-export')
-          .find('i')
-          .should('have.class', 'mdi-download');
-      });
+    coreTests();
+  });
+
+  context('mobile', () => {
+    beforeEach(() => {
+      setActivePinia(createPinia());
+      cy.mount(HeaderOrganization);
+      cy.viewport('iphone-6');
     });
+
+    coreTests();
   });
 });
+
+function coreTests() {
+  testData.forEach((test) => {
+    it(test.description, () => {
+      // initiate store state
+      cy.wrap(useAdminOrganisationStore()).then((adminOrganisationStore) => {
+        const adminOrganisations = computed(
+          () => adminOrganisationStore.getAdminOrganisations,
+        );
+        adminOrganisationStore.setAdminOrganisations(test.storeData);
+        cy.wrap(adminOrganisations)
+          .its('value')
+          .should('deep.equal', test.storeData);
+      });
+      // test icon
+      cy.dataCy('header-organization-image-icon').should('be.visible');
+      cy.dataCy('header-organization-image-icon').should(
+        'have.color',
+        secondary,
+      );
+      // test organization data
+      cy.dataCy('header-organization').should('be.visible');
+      cy.dataCy('header-organization-title')
+        .should('be.visible')
+        .and('contain', test.displayData.organizationTitle);
+      // test subsidiaries count
+      cy.dataCy('header-organization-branch-count')
+        .should('be.visible')
+        .and('contain', test.displayData.subsidiariesCount)
+        .and(
+          'contain',
+          i18n.global.t(
+            'coordinator.labelBranches',
+            test.displayData.subsidiariesCount,
+          ),
+        );
+      // test members count
+      cy.dataCy('header-organization-member-count')
+        .should('be.visible')
+        .and('contain', test.displayData.membersCount)
+        .and(
+          'contain',
+          i18n.global.t(
+            'coordinator.labelMembers',
+            test.displayData.membersCount,
+          ),
+        );
+      // test export button
+      cy.dataCy('header-organization-button-export')
+        .should('be.visible')
+        .and('contain', i18n.global.t('coordinator.buttonExportMembers'));
+      cy.dataCy('header-organization-button-export')
+        .find('i')
+        .should('have.class', 'mdi-download');
+    });
+  });
+}
