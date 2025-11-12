33f933c063f5d05f221e4cc009b47b7555fc2f7a
Tomas Zigo <tomas.zigo@slovanet.sk>
2025-11-12 06:30:54 +0100

    Fix Cypress command func param doc description

--- test/cypress/support/commands/store_commands.js
+++ test/cypress/support/commands/store_commands.js
@@ -2,6 +2,10 @@ import { computed } from 'vue';
 
 /**
  * Set a store value and wait for it to register
+ *
+ * @param {function} useRegisterChallengeStore - `useRegisterChallengeStore` function
+ *                                                to ininitialize register challenge store
+ * @param {boolean} expectedValue - `true` for making user organization admin
  */
 Cypress.Commands.add(
   'setIsUserOrganizationAdminStoreValue',
