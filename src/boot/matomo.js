74ca8cce930f3cdce540a70cbd76f189f200d55f
Tomas Zigo <tomas.zigo@slovanet.sk>
2026-03-11 08:57:42 +0100

    src/boot: enable Matomo Analytics tracking Vue.js lib

---
+++ src/boot/matomo.js
@@ -0,0 +1,21 @@
+import { boot } from 'quasar/wrappers';
+
+import { initMatomo } from '@certible/use-matomo';
+import { rideToWorkByBikeConfig } from './global_vars';
+
+/**
+ * Matomo Analytics integration for Quasar.
+ *
+ * @see https://www.npmjs.com/package/@certible/use-matomo
+ * @see https://matomo.org/
+ */
+export default boot(({ app }) => {
+  const matomo = initMatomo({
+    host: rideToWorkByBikeConfig.urlMatomoAnalytics,
+    siteId: 1,
+    enableLinkTracking: true,
+    requireConsent: true, // GDPR compliance
+    trackRouter: true, // Automatic SPA page changes via history tracking
+  });
+  app.provide('matomo', matomo);
+});
